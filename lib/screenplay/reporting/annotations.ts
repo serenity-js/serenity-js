267d5011ae4daf95b00d620477a1c288e39ebd5b
jan-molak <jan.molak@gmail.com>
2016-06-20 02:28:07 +0100

    @step can only be applied to Performables now

--- lib/screenplay/reporting/annotations.ts
+++ lib/screenplay/reporting/annotations.ts
@@ -1,8 +1,7 @@
 import {Performable} from "../pattern/performables";
-import {PerformsTasks} from "../pattern/actor";
-import {Enter} from "../../screenplay_protractor/actions/enter";
 
-export function step(stepDescription: string) {
+export function step<STEP extends Performable>(stepDescriptionTemplate: string) {
+
     function using(source: any) {
         return (token: string, field: string|number) => {
             switch({}.toString.call(source[field])) {
@@ -14,27 +13,29 @@ export function step(stepDescription: string) {
         }
     }
 
-    function interpolate(currentPerformable: any, performAsMethodArguments: any[]) {
+    function interpolateStepDescription(currentPerformable: Performable, performAsMethodArguments: any[]) {
         const argToken = /{(\d+)}/g,
             fieldToken = /#(\w+)/g;
 
-        return stepDescription.
+        return stepDescriptionTemplate.
             replace(fieldToken, using(currentPerformable)).
             replace(argToken, using(performAsMethodArguments))
     }
 
-    return (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<(actor: PerformsTasks) => void>) => {
+    return (target: STEP, propertyKey: string, descriptor: TypedPropertyDescriptor<(any) => void>) => {
+
         let step = descriptor.value;
 
         descriptor.value = function(...args: any[]): void {
 
-            // let interpolated = stepDescription.replace(fieldToken, using(this)).replace(argToken, using(args));
-            let interpolated = interpolate(this, args);
+            let interpolated = interpolateStepDescription(this, args);
 
+            // todo: send "step started event"
             console.log('[PERFORMING STEP] ', interpolated);
 
             step.apply(this, args);
 
+            // todo: send "step finished event"
             console.log('[  FINISHED STEP] ', interpolated);
         };
 
