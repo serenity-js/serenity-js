54189e85746cc27533c028fe5ac8f29ed733560e
jan-molak <jan.molak@gmail.com>
2016-12-21 01:04:22 +0000

    test(Moved automated tests from phantomjs to browserstack):

--- cookbook/protractor.conf.js
+++ cookbook/protractor.conf.js
@@ -5,15 +5,32 @@ var path         = require('path'),
     protractor   = require.resolve('protractor'),
     node_modules = protractor.substring(0, protractor.lastIndexOf('node_modules') + 12);
 
-exports.config = {
-    seleniumServerJar: path.resolve(node_modules, 'protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar'),
+// var local = {
+//     seleniumServerJar: path.resolve(node_modules, 'protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar'),
+//
+//     framework: 'mocha',
+//     specs: [ 'recipes/**/*.recipe.ts' ],
+//
+//     capabilities: {
+//         'browserName': 'chrome'
+//     },
+//
+//     restartBrowserBetweenTests: false
+// };
 
+exports.config = {
     framework: 'mocha',
     specs: [ 'recipes/**/*.recipe.ts' ],
 
+    browserstackUser: process.env.BROWSERSTACK_USERNAME,
+    browserstackKey:  process.env.BROWSERSTACK_KEY,
+
     capabilities: {
-        'browserName': 'chrome',
-        'phantomjs.binary.path': path.resolve(node_modules, 'phantomjs-prebuilt/lib/phantom/bin/phantomjs'),
+        browserName:          'chrome',
+        'version':            'ANY',
+        build:                process.env.BROWSERSTACK_AUTOMATE_BUILD,
+        project:              process.env.BROWSERSTACK_AUTOMATE_PROJECT,
+        'browserstack.local': true,
     },
 
     restartBrowserBetweenTests: false
