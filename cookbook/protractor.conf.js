5126e1a8d80c123b5add7fc7e8d42eb75761879d
jan-molak <jan.molak@gmail.com>
2016-12-21 02:54:52 +0000

    test(travis): Only run the BrowserStack tests on CI, use local browser for local testing

--- cookbook/protractor.conf.js
+++ cookbook/protractor.conf.js
@@ -5,20 +5,9 @@ var path         = require('path'),
     protractor   = require.resolve('protractor'),
     node_modules = protractor.substring(0, protractor.lastIndexOf('node_modules') + 12);
 
-// var local = {
-//     seleniumServerJar: path.resolve(node_modules, 'protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar'),
-//
-//     framework: 'mocha',
-//     specs: [ 'recipes/**/*.recipe.ts' ],
-//
-//     capabilities: {
-//         'browserName': 'chrome'
-//     },
-//
-//     restartBrowserBetweenTests: false
-// };
-
 exports.config = {
+    seleniumServerJar: path.resolve(node_modules, 'protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar'),
+
     framework: 'mocha',
     specs: [ 'recipes/**/*.recipe.ts' ],
 
