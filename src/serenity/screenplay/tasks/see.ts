4feacf04c8879ff283ee77d8f4f11aa2b4810ba9
jan-molak <jan.molak@gmail.com>
2017-01-19 07:59:37 +0000

    refactor(screenplay): Type AssertionFn renamed to Expactation, which feels more descriptive.

--- src/serenity/screenplay/tasks/see.ts
+++ src/serenity/screenplay/tasks/see.ts
@@ -1,18 +1,18 @@
 import { AnswersQuestions, Performable, Question } from '..';
 import { step } from '../../recording/step_annotation';
 
-export type AssertionFn<S> = (subject: S) => PromiseLike<void>;
+export type Expectation<S> = (subject: S) => PromiseLike<void>;
 
 export class See<S> implements Performable {
-    static that<S>(subject: Question<S>, verifier: AssertionFn<S>) {
+    static that<S>(subject: Question<S>, verifier: Expectation<S>) {
         return new See(subject, verifier);
     }
 
     @step('{0} looks at #question')
     performAs(actor: AnswersQuestions): PromiseLike<void> {
-        return this.assert(actor.toSee(this.question));
+        return this.expect(actor.toSee(this.question));
     }
 
-    constructor(private question: Question<S>, private assert: AssertionFn<S>) {
+    constructor(private question: Question<S>, private expect: Expectation<S>) {
     }
 }
