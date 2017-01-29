ed4fabb6a3a41f901df55284d37dad979fa59b5f
jan-molak <jan.molak@gmail.com>
2017-01-29 23:26:49 +0000

    fix(core): JSONObject interface definition should not reference the static `JSON` class

--- src/serenity/io/json.ts
+++ src/serenity/io/json.ts
@@ -1,5 +1,5 @@
 export interface JSONObject {
-    [x: string]: string | number | boolean | Date | JSON | JSONArray;
+    [x: string]: string | number | boolean | Date | JSONObject | JSONArray;
 }
 
 export interface JSONArray extends Array<string | number | boolean | Date | JSONObject | JSONArray> { }
