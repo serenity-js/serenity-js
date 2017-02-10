1c4a07a8e90ef227f02d6e4cdbd043d6b308fca6
jan-molak <jan.molak@gmail.com>
2017-02-10 17:25:21 +0000

    docs(handbook): Corrected the RSS feed urls

--- book.js
+++ book.js
@@ -65,7 +65,7 @@ module.exports = {
             title: 'Serenity/JS Handbook',
             description: 'A comprehensive reference manual and tutorials for Serenity/JS, a next generation automated testing library.',
             author: 'Jan Molak',
-            site_url: 'http://serenity-js.org',
+            site_url: 'http://serenity-js.org/',
             feed_url: 'http://serenity-js.org/updates.rss',
             image_url: 'http://serenity-js.org/assets/images/serenity-js-eye.svg',
             managingEditor: 'jan.molak@smartcodeltd.co.uk (Jan Molak)',
