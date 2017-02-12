b5f1c7430e95dbefe4fe80394bac21c9f2763aea
jan-molak <jan.molak@gmail.com>
2017-02-12 21:37:13 +0000

    perf(serenity-mocha, serenity-cucumber): Wait up to 10 seconds for the browser to be restarted and r
    To ensure enough time for a browser to be restarted and the screenshots from the previous scenario
    to be saved to disk, Serenity/JS will wait up to 10 seconds before starting with a scenario.

--- src/serenity-mocha/stage_cue.ts
+++ src/serenity-mocha/stage_cue.ts
@@ -2,4 +2,7 @@ import { serenity } from '../index';
 
 // wait for any tasks outstanding after the previous scenario
 // see https://github.com/angular/protractor/issues/4087
-beforeEach(() => serenity.stageManager().waitForNextCue());
+beforeEach(function() {
+  this.timeout(10 * 1000);
+  return serenity.stageManager().waitForNextCue();
+});
