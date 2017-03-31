d0fa17e443c123b1ca9724bd0a5388569ac7b280
jan-molak <jan.molak@gmail.com>
2017-03-31 11:10:10 +0100

    fix(browserstack): increase default timeout to 30s to allow for the screenshots to be downloaded fro
    affects: serenity-js
    
    This is a temporary patch to address #34, a configurable version is on the way.

--- packages/serenity-js/src/serenity-cucumber/stage_cue.ts
+++ packages/serenity-js/src/serenity-cucumber/stage_cue.ts
@@ -3,5 +3,5 @@ import { serenity } from '../index';
 // wait for any tasks outstanding after the previous scenario
 // see https://github.com/angular/protractor/issues/4087
 export = function() {
-    this.Before({timeout: 10 * 1000}, () => serenity.stageManager().waitForNextCue());
+    this.Before({timeout: 30 * 1000}, () => serenity.stageManager().waitForNextCue());
 };
