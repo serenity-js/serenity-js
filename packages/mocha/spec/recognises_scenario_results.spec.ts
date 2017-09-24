f743361679322ea1f1fe13a4573b269d3c959d39
jan-molak <jan.molak@gmail.com>
2017-09-24 23:50:25 +0100

    chore(ci): adjusted the timeouts as Travis seems to be taking its time
    affects: @serenity-js/mocha

--- packages/mocha/spec/recognises_scenario_results.spec.ts
+++ packages/mocha/spec/recognises_scenario_results.spec.ts
@@ -6,7 +6,7 @@ import { mocha } from './mocha/spawner';
 
 describe('When working with Mocha', function() {
 
-    this.timeout(5 * 1000);
+    this.timeout(10 * 1000);
 
     describe('Serenity/JS', () => {
 
