d67fa111ff4efa0540f3ccd53e501ac2c8655547
jan-molak <jan.molak@gmail.com>
2017-10-19 01:17:50 +0100

    style(linter): corrected linting errors
    affects: serenity-js

--- packages/serenity-js/src/serenity-protractor/screenplay/interactions/alert.ts
+++ packages/serenity-js/src/serenity-protractor/screenplay/interactions/alert.ts
@@ -3,9 +3,9 @@ import { BrowseTheWeb } from '../abilities/browse_the_web';
 
 export class Alert {
     static accept = () => Interaction.where(`#actor accepts the alert popup`,
-        actor => BrowseTheWeb.as(actor).acceptAlert()
+        actor => BrowseTheWeb.as(actor).acceptAlert(),
     );
     static dismiss = () => Interaction.where(`#actor dismisses the alert popup`,
-        actor => BrowseTheWeb.as(actor).dismissAlert()
+        actor => BrowseTheWeb.as(actor).dismissAlert(),
     );
 }
