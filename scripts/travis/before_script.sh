71c3a454dbab974671843e75ce73e2e94183d948
jan-molak <jan.molak@gmail.com>
2017-10-29 02:23:59 +0000

    chore(ci): added XVFB as apparently "headless" chrome needs it.
    According to the build error message, an X display is required for keycode conversions

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -3,3 +3,9 @@ set -e
 
 echo "Removing stale node_modules"
 npm run lerna exec --loglevel info -- npm prune
+
+echo "Starting XVFB"
+
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+sleep 3 # give xvfb some time to start
