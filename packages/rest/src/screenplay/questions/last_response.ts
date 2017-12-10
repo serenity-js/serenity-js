f704d9cee8ce31325f1f495168ffb4adc3d4f79c
Kenny Baas <kbaas@xebia.com>
2017-12-10 10:50:59 +0100

    Added index for question
    Fixed tslint errors

--- packages/rest/src/screenplay/questions/last_response.ts
+++ packages/rest/src/screenplay/questions/last_response.ts
@@ -1,6 +1,6 @@
-import {CallAnApi} from '../abilities';
 import {Question} from '@serenity-js/core/lib/screenplay';
 import {AxiosResponse} from 'axios';
+import {CallAnApi} from '../abilities';
 
 export const LastResponse = () => Question.about(`The Message`, actor =>
     CallAnApi.as(actor).getLastResponse() as PromiseLike<AxiosResponse>,
