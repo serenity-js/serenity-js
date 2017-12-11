c19e0ca9a42dd49e9a53f327f6a50e7cd49d413a
Kenny Baas <kbaas@xebia.com>
2017-12-11 15:10:38 +0100

    Added tests for post Call an Api function

--- packages/rest/spec/screenplay/abilities/call_an_api.spec.ts
+++ packages/rest/spec/screenplay/abilities/call_an_api.spec.ts
@@ -40,5 +40,36 @@ describe('Abilities', () => {
             it('and LastResponse has no data', () => callAnApi.getLastResponse().then(response => expect(response.data).to.be.empty));
 
         });
+
+        describe('performs a post on a resource and get 201', () => {
+            const resource = '/201';
+            const dummyRest = nock(baseUrl)
+                .post(resource)
+                .reply(201, {
+                    username: '1337',
+                });
+
+            before(() => expect(callAnApi.post(resource, {username: '1337'})).to.eventually.be.fulfilled);
+
+            it('and eventually has a LastResponse', () => expect(callAnApi.getLastResponse()).to.eventually.not.be.undefined);
+            it('and LastResponse status is 404', () => callAnApi.getLastResponse().then(response => expect(response.status).to.equal(201)));
+            it('and LastResponse has no data', () => callAnApi.getLastResponse().then(response => expect(response.data).to.not.be.empty));
+
+        });
+
+        describe('performs a post on a resource and get 403', () => {
+            const resource = '/403';
+            const dummyRest = nock(baseUrl)
+                .get(resource)
+                .reply(403);
+
+            before(() => expect(callAnApi.get(resource)).to.eventually.be.fulfilled);
+
+            it('and eventually has a LastResponse', () => expect(callAnApi.getLastResponse()).to.eventually.not.be.undefined);
+            it('and LastResponse status is 404', () => callAnApi.getLastResponse().then(response => expect(response.status).to.equal(403)));
+            it('and LastResponse has no data', () => callAnApi.getLastResponse().then(response => expect(response.data).to.be.empty));
+
+        });
+
     });
 });
