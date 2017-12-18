db8b6ff6d06cd8390d6e2c5ad6c1ebda4b7d715e
jan-molak <jan.molak@gmail.com>
2017-12-18 13:10:39 +0100

    chore(ci): debugging why the phantomjs-prebuilt module is not getting installed

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -4,6 +4,12 @@ set -e
 echo "Removing stale node_modules"
 npm run lerna exec --loglevel info -- npm prune
 
+echo "[DEBUG] list node_modules"
+ls -lah node_modules/
+
+echo "[DEBUG] list node_modules"
+ls -lah packages/serenity-js/node_modules/
+
 echo "Starting XVFB"
 
 export DISPLAY=:99.0
