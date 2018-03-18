89c957b01df292f3b736d0bbb4db6185f559f763
jan-molak <jan.molak@gmail.com>
2018-03-18 01:18:57 +0000

    chore(ci): replaced PhantomJS with Headless Chrome
    affects: @serenity-js/mocha, serenity-js
    
    This change simplifies the build as Headless Chrome is now used instead of the PhantomJS /
    BrowserStack combo
    PhantomJS had bugs that prevented it from running some of the tests (like the
    multi-select for example) and TravisCI doesn't allow
    for BrowserStack tests to be executed for
    pull requests, which means that a PR build wouldn't have full test coverage.
    
    ISSUES CLOSED: Addresses #89 and partially #137

--- packages/serenity-js/spec/integration/mocha/protractor.conf.js
+++ packages/serenity-js/spec/integration/mocha/protractor.conf.js
@@ -2,13 +2,17 @@
 
 require('ts-node/register');    // because of the child_process_reporter.ts
 
-let path         = require('path'),
+const
+    isCI         = require('../../isCI'),
+    path         = require('path'),
     protractor   = require.resolve('protractor'),
     node_modules = protractor.substring(0, protractor.lastIndexOf('node_modules') + 12),
     crew         = require('../../support/child_process_reporter.ts');
 
 exports.config = {
-    seleniumServerJar: path.resolve(node_modules, 'protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar'),
+    // seleniumServerJar: path.resolve(node_modules, 'protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar'),
+
+    directConnect: true,
 
     framework: 'custom',
 
@@ -26,7 +30,11 @@ exports.config = {
     },
 
     capabilities: {
-        'browserName': 'phantomjs',
-        'phantomjs.binary.path': path.resolve(node_modules, 'phantomjs-prebuilt/bin/phantomjs'),
+        browserName: 'chrome',
+
+        chromeOptions: {
+            args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+                .concat(isCI() ? ['--no-sandbox'] : [])
+        }
     },
 };
