4f9a54b61979ec8fd8dfda93d94ca674f1b02034
Jan Molak <jan.molak@smartcodeltd.co.uk>
2018-08-30 17:55:44 +0100

    test(core): additional test coverage for @serenity-js/core/lib/io
    affects: @serenity-js/core

---
+++ packages/core/spec/io/FileSystemLocation.spec.ts
@@ -0,0 +1,21 @@
+import 'mocha';
+import { given } from 'mocha-testdata';
+import { Serialised } from 'tiny-types';
+
+import { FileSystemLocation, Path } from '../../src/io';
+import { expect } from '../expect';
+
+describe ('FileSystemLocation', () => {
+
+    const path = new Path('/home/jan/file.json');
+
+    given(
+        { description: 'no line or column',                     location: new FileSystemLocation(path)          },
+        { description: 'line available',                        location: new FileSystemLocation(path, 10)      },
+        { description: 'line and column available available',   location: new FileSystemLocation(path, 10, 3)   },
+    ).
+    it('can be serialised and deserialised', ({ location }) => {
+        expect(FileSystemLocation.fromJSON(location.toJSON() as Serialised<FileSystemLocation>))
+            .to.equal(location);
+    });
+});
