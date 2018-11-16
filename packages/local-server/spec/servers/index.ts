13a9204e120289942f0bb07f793c00e18719b020
Jan Molak <jan.molak@smartcodeltd.co.uk>
2018-11-16 01:03:45 +0000

    chore(local-server): run HAPI tests on node >= 8.12
    affects: @serenity-js/local-server

--- packages/local-server/spec/servers/index.ts
+++ packages/local-server/spec/servers/index.ts
@@ -1,8 +1,14 @@
+import { satisfies } from 'semver'; // tslint:disable-line:no-implicit-dependencies
+
+function node(requiredVersion: string) {
+    return satisfies(process.versions.node, requiredVersion);
+}
+
 // tslint:disable:no-var-requires
 export = [
     require('./barebones'),
     require('./express'),
-    require('./hapi'),
+    node('>= 8.12') && require('./hapi'),
     require('./koa'),
     require('./restify'),
-];
+].filter(_ => !! _);
