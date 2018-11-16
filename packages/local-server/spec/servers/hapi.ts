e69af2555dd4dce8a5c412315a86dc65277ecbff
Jan Molak <jan.molak@smartcodeltd.co.uk>
2018-11-16 12:25:09 +0000

    test(local-server): disabled the HAPI test on node 6
    affects: @serenity-js/local-server

--- packages/local-server/spec/servers/hapi.ts
+++ packages/local-server/spec/servers/hapi.ts
@@ -1,10 +1,10 @@
 // https://github.com/hapijs/hapi
-import * as hapi from 'hapi';
 
 export = {
     node: '>= 8.12',
     description: 'Hapi app',
     handler: () => {
+        const hapi = require('hapi');   // tslint:disable-line:no-var-requires Requiring Hapi breaks the build on Node 6
         const server = new hapi.Server();
 
         server.route({ method: 'GET', path: '/', handler: (req, h) => 'Hello World!' });
