9fdabe2fa704f1ece1afedcd099b1a6e088ad2db
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-01-31 10:07:36 +0000

    ci(ci): Disabled gitbook as we don't need it and corrected Lerna publish command

--- scripts/travis/after_script.sh
+++ scripts/travis/after_script.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "Pull requests do not affect the public Serenity/JS Handbook"
-  npm run prebook:publish
-  exit 0
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#  echo "Pull requests do not affect the public Serenity/JS Handbook"
+#  npm run prebook:publish
+#  exit 0
+#fi
 
 if [[ $TRAVIS_BRANCH == '2.0' ]]; then
-    make site
-fi
-
-if [[ $TRAVIS_BRANCH != 'master' ]]; then
-  echo "Building from a branch does not affect the public Serenity/JS Handbook"
-  npm run prebook:publish
-  exit 0
+   make site
 fi
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
