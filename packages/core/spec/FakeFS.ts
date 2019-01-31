a999069f92832e1c5c1e226c10846684ce404155
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-01-31 11:56:24 +0000

    test(core): Migrated from mock-fs to memfs
    Mock-fs doesn't support the latest Node LTS

---
+++ packages/core/spec/FakeFS.ts
@@ -0,0 +1,43 @@
+import fs = require('fs');
+import path = require('path');
+const { createFsFromVolume, Volume } = require('memfs'); // tslint:disable-line:no-var-requires Typings incorrectly assume the presence of "dom" lib
+
+export interface DirectoryStructure {
+    [ key: string ]: DirectoryStructure | string;
+}
+
+export class FakeFS {
+    public static readonly Empty_Directory = {};
+
+    static with(tree: DirectoryStructure, cwd = process.cwd()): typeof fs {
+
+        function flatten(
+            currentStructure: DirectoryStructure | string,
+            currentPath: string = '',
+        ): { [key: string]: string } {
+
+            if (typeof currentStructure === 'string') {
+                return ({
+                    [ currentPath ]: currentStructure,
+                });
+            }
+
+            const entries = Object.keys(currentStructure);
+
+            if (! entries.length) {
+                return ({
+                    [ currentPath ]: void 0,
+                });
+            }
+
+            return Object.keys(currentStructure).reduce((acc, key) =>  {
+                return ({
+                    ...acc,
+                    ...flatten(currentStructure[key], path.join(currentPath, key)),
+                });
+            }, {});
+        }
+
+        return createFsFromVolume(Volume.fromJSON(flatten(tree), cwd)) as typeof fs;
+    }
+}
