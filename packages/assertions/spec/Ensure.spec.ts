b2e41988d30f3d354450fee507da3b0b31203d6e
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-02-13 16:20:34 +0000

    fix(core): `formatted` tag function correctly removes new line characters from the output

--- packages/assertions/spec/Ensure.spec.ts
+++ packages/assertions/spec/Ensure.spec.ts
@@ -3,7 +3,7 @@ import { given } from 'mocha-testdata';
 
 import { expect } from '@integration/testing-tools';
 import { Actor, AssertionError, KnowableUnknown } from '@serenity-js/core';
-import { Ensure } from '../src';
+import { Ensure, equals } from '../src';
 import { isIdenticalTo, p, q } from './fixtures';
 
 /** @test {Ensure} */
@@ -30,12 +30,22 @@ describe('Ensure', () => {
         expect(Ensure.that(4, isIdenticalTo(7)).toString()).to.equal(`#actor ensures that 4 does have value identical to 7`);
     });
 
+    /** @test {Ensure.that} */
+    it('provides a description of the assertion being made, while correctly cleaning the output from new line characters', () => {
+        expect(Ensure.that({ person: { name: 'Jan' }}, equals({
+            person: {
+                name: 'Jan',
+            },
+        })).toString()).to.equal(`#actor ensures that { person: { name: 'Jan' } } does equal { person: { name: 'Jan' } }`);
+    });
+
     given<KnowableUnknown<number>>(
         42,
         p(42),
         q(42),
         q(p(42)),
     ).
+    /** @test {Ensure.that} */
     it('allows for the actual to be a KnowableUnknown<T> as it compares its value', (actual: KnowableUnknown<number>) => {
         return expect(Enrique.attemptsTo(
             Ensure.that(actual, isIdenticalTo(42)),
