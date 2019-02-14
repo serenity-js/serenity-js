abbac184e44848bc5fa9b8e2fd6ea1f9b4973035
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-02-14 10:25:51 +0000

    feat(assertions): Check.whether enables conditional flow

--- integration/testing-tools/src/expect/index.ts
+++ integration/testing-tools/src/expect/index.ts
@@ -1,8 +1,10 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
+import * as sinonChai from 'sinon-chai';
 import { assertions } from './tiny-types';
 
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
 chai.use(assertions);
 
 export const expect = chai.expect;
