3dec5e38ed2c5725dce94a0ea3c27c0f89b17d4d
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-03-05 17:20:13 +0000

    feat(protractor): Scroll.to interaction

---
+++ packages/protractor/src/screenplay/interactions/Scroll.ts
@@ -0,0 +1,24 @@
+import { AnswersQuestions, Interaction, Question, UsesAbilities } from '@serenity-js/core';
+import { formatted } from '@serenity-js/core/lib/io';
+import { ElementFinder } from 'protractor';
+import { BrowseTheWeb } from '../abilities';
+import { withAnswerOf } from '../withAnswerOf';
+
+export class Scroll implements Interaction {
+    static to(target: Question<ElementFinder> | ElementFinder) {
+        return new Scroll(target);
+    }
+
+    constructor(
+        private readonly target: Question<ElementFinder> | ElementFinder,
+    ) {
+    }
+
+    performAs(actor: UsesAbilities & AnswersQuestions): PromiseLike<void> {
+        return withAnswerOf(actor, this.target, elf => BrowseTheWeb.as(actor).actions().mouseMove(elf).perform());
+    }
+
+    toString(): string {
+        return formatted `#actor scrolls to ${this.target}`;
+    }
+}
