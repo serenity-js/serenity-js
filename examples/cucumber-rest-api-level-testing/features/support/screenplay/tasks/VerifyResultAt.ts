d905cba0c35e383d1abdb687f74f247992bd83aa
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-04-04 10:22:27 +0100

    feat(core): Serenity/JS reports are compatible with Serenity BDD CLI 2.1.8

--- examples/cucumber-rest-api-level-testing/features/support/screenplay/tasks/VerifyResultAt.ts
+++ examples/cucumber-rest-api-level-testing/features/support/screenplay/tasks/VerifyResultAt.ts
@@ -1,8 +1,8 @@
 import { Ensure, equals, Expectation } from '@serenity-js/assertions';
-import { KnowableUnknown, Task } from '@serenity-js/core';
+import { Answerable, Task } from '@serenity-js/core';
 import { GetRequest, LastResponse, Send } from '@serenity-js/rest';
 
-export const VerifyResultAt = (url: KnowableUnknown<string>, expectation: Expectation<KnowableUnknown<any>>) =>
+export const VerifyResultAt = (url: Answerable<string>, expectation: Expectation<Answerable<any>>) =>
     Task.where(`#actor verifies result at ${ url }`,
         Send.a(GetRequest.to(url)),
         Ensure.that(LastResponse.status(), equals(200)),
