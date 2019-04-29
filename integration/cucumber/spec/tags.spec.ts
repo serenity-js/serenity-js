2563c5a6d9e2e87f0178ccb9831d5278a0750cf7
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-04-29 15:36:44 +0100

    fix(cucumber): Corrected how scenario and feature-level @issue and @issues tags are reported

--- integration/cucumber/spec/tags.spec.ts
+++ integration/cucumber/spec/tags.spec.ts
@@ -36,8 +36,8 @@ describe('@serenity-js/cucumber', function () {
             PickEvent.from(res.events)
                 .next(SceneStarts,  event => expect(event.value.name.value).to.equal('A tagged scenario'))
                 .next(SceneTagged,  event => expect(event.tag).to.equal(new FeatureTag('Serenity/JS recognises tags at multiple levels')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@feature-tag')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@scenario-tag')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('feature-tag')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('scenario-tag')))
             ;
         }));
 
@@ -68,14 +68,14 @@ describe('@serenity-js/cucumber', function () {
             PickEvent.from(res.events)
                 .next(SceneStarts,  event => expect(event.value.name.value).to.equal('More tagged scenarios'))
                 .next(SceneTagged,  event => expect(event.tag).to.equal(new FeatureTag('Serenity/JS recognises tags at multiple levels')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@feature-tag')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@scenario-outline-tag')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@example-set-1')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('feature-tag')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('scenario-outline-tag')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('example-set-1')))
                 .next(SceneStarts,  event => expect(event.value.name.value).to.equal('More tagged scenarios'))
                 .next(SceneTagged,  event => expect(event.tag).to.equal(new FeatureTag('Serenity/JS recognises tags at multiple levels')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@feature-tag')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@scenario-outline-tag')))
-                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('@example-set-2')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('feature-tag')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('scenario-outline-tag')))
+                .next(SceneTagged,  event => expect(event.tag).to.equal(new ArbitraryTag('example-set-2')))
             ;
         }));
 });
---
+++ packages/cucumber/spec/gherkin/feature-files/fixtures/tags.feature
@@ -0,0 +1,9 @@
+@issues:ABC-123,DEF-456
+@saves-data
+Feature: Sample feature
+
+  @issue:GHI-789
+  @regression
+  Scenario: Sample scenario
+
+    Given some step
