3518cacbd158091c67e798e3e7f819288201c7fb
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-06-22 00:01:31 +0100

    docs(core): Documented the "errors" package

--- packages/core/src/errors/AssertionError.ts
+++ packages/core/src/errors/AssertionError.ts
@@ -1,6 +1,19 @@
 import { RuntimeError } from './RuntimeError';
 
+/**
+ * @desc
+ *  Thrown to indicate that an assertion has failed.
+ *
+ * @extends {RuntimeError}
+ */
 export class AssertionError extends RuntimeError {
+
+    /**
+     * @param {string} message - Human-readable description of the error
+     * @param {any} expected - The value that was expected
+     * @param {any} actual - The value that was received instead of the expected one
+     * @param {Error} [cause] - The root cause of this {@link RuntimeError}, if any
+     */
     constructor(message: string, public readonly expected: any, public readonly actual: any, cause?: Error) {
         super(AssertionError, message, cause);
     }
--- packages/core/src/errors/UnknownError.ts
+++ packages/core/src/errors/UnknownError.ts
@@ -1,6 +1,17 @@
 import { RuntimeError } from './RuntimeError';
 
+/**
+ * @desc
+ *  Thrown to indicate that an unknown error has occurred.
+ *
+ * @extends {RuntimeError}
+ */
 export class UnknownError extends RuntimeError {
+
+    /**
+     * @param {string} message - Human-readable description of the error
+     * @param {Error} [cause] - The root cause of this {@link RuntimeError}, if any
+     */
     constructor(message: string, cause?: Error) {
         super(UnknownError, message, cause);
     }
