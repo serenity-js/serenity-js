288011696bd1825ee5ae61d00e265f5d1706a1c2
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-09-01 01:43:34 +0100

    feat(protractor): Photographer.whoWill(..) factory method to make instantiation of the Photographer
    #335

--- packages/protractor/src/stage/photographer/strategies/PhotoTakingStrategy.ts
+++ packages/protractor/src/stage/photographer/strategies/PhotoTakingStrategy.ts
@@ -11,8 +11,27 @@ import {
 import { CorrelationId, Description, Name, Photo } from '@serenity-js/core/lib/model';
 import { BrowseTheWeb } from '../../../screenplay';
 
+/**
+ * @desc
+ *  Configures the {@link Photographer} to take photos (a.k.a. screenshots)
+ *  of the {@link @serenity-js/core/lib/screenplay~Activity} performed
+ *  by the {@link @serenity-js/core/lib/screenplay/actor~Actor} in the spotlight
+ *  under specific conditions.
+ *
+ * @abstract
+ */
 export abstract class PhotoTakingStrategy {
 
+    /**
+     * @desc
+     *  Takes a photo of the web browser held by the {@link @serenity-js/core/lib/screenplay/actor~Actor} in the spotlight.
+     *
+     * @param {@serenity-js/core/lib/events~ActivityStarts | @serenity-js/core/lib/events~ActivityFinished} event
+     * @param {@serenity-js/core/lib/stage~Stage} stage - the Stage that holds reference to the Actor in the spotlight
+     * @returns void
+     *
+     * @see {@serenity-js/core/lib/stage~Stage#theActorInTheSpotlight}
+     */
     considerTakingPhoto(event: ActivityStarts | ActivityFinished, stage: Stage): void {
         if (this.shouldTakeAPhotoOf(event)) {
             const
--- packages/protractor/src/stage/photographer/strategies/TakePhotosOfInteractions.ts
+++ packages/protractor/src/stage/photographer/strategies/TakePhotosOfInteractions.ts
@@ -1,6 +1,19 @@
 import { DomainEvent, InteractionFinished } from '@serenity-js/core/lib/events';
 import { PhotoTakingStrategy } from './PhotoTakingStrategy';
 
+/**
+ * @desc
+ *  Configures the {@link Photographer} to take photos (a.k.a. screenshots) when
+ *  the the {@link @serenity-js/core/lib/screenplay/actor~Actor} in the spotlight
+ *  performs any {@link @serenity-js/core/lib/screenplay~Interaction}.
+ *
+ *  This strategy works best when you want the results of your automated tests
+ *  to become comprehensive living documentation of your system.
+ *
+ *  *Please note* that taking screenshots affects the performance of your tests.
+ *
+ * @implements {PhotoTakingStrategy}
+ */
 export class TakePhotosOfInteractions extends PhotoTakingStrategy {
     protected shouldTakeAPhotoOf(event: DomainEvent): boolean {
         return event instanceof InteractionFinished;
