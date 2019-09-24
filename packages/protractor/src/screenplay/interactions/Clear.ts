f7908a8e6142e66f5c7a70fe8811cdd3a732da05
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-09-24 12:44:42 +0100

    fix(protractor): The Clear interaction willl complain if used with an element that cannot be cleared
    Thanks to @Ethan-G for reporting the issue!

--- packages/protractor/src/screenplay/interactions/Clear.ts
+++ packages/protractor/src/screenplay/interactions/Clear.ts
@@ -1,4 +1,4 @@
-import { AnswersQuestions, Interaction, Question, UsesAbilities } from '@serenity-js/core';
+import { AnswersQuestions, Interaction, LogicError, Question, UsesAbilities } from '@serenity-js/core';
 import { formatted } from '@serenity-js/core/lib/io';
 import { ElementFinder, protractor } from 'protractor';
 import { withAnswerOf } from '../withAnswerOf';
@@ -14,16 +14,32 @@ export class Clear extends Interaction {
 
     performAs(actor: UsesAbilities & AnswersQuestions): PromiseLike<void> {
         return withAnswerOf(actor, this.field, elf =>
-            elf.getAttribute('value').then(value =>
-                elf.sendKeys(
-                    protractor.Key.END,
-                    ...this.times(value.length, protractor.Key.BACK_SPACE),
-                ),
-            ));
+            elf.getAttribute('value').then(value => {
+                if (! value) {
+                    throw new LogicError(
+                        `${ this.capitaliseFirstLetter(this.field.toString()) } doesn't seem to have a 'value' attribute that could be cleared.`,
+                    );
+                }
+
+                return this.removeCharactersFrom(elf, value.length);
+            }));
     }
 
     toString(): string {
         return formatted `#actor clears the value of ${ this.field }`;
+    }
+
+    private capitaliseFirstLetter(text: string) {
+        return text.charAt(0).toUpperCase() + text.substring(1);
+    }
+
+    private removeCharactersFrom(elf: ElementFinder, numberOfCharacters: number): PromiseLike<void> {
+        return numberOfCharacters === 0
+            ? Promise.resolve(void 0)
+            : elf.sendKeys(
+                protractor.Key.END,
+                ...this.times(numberOfCharacters, protractor.Key.BACK_SPACE),
+            );
     }
 
     private times(n: number, key: string) {
