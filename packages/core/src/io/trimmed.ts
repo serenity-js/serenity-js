f096df774f9ca5fe5ec2e02427b1e887d0ec2f8e
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-11-24 03:28:03 +0000

    fix(core): ErrorSerialiser correctly interprets assertion errors reported by Jasmine
    Instead of reporting an _error message_, Jasmine reports a _display message_, optimised for a human
    reader rather than an automated process. Fixed the ErrorSerialiser so that it correctly recognises
    those messages. | Added a `trimmed` function to help formatting multi-line strings easier

---
+++ packages/core/src/io/trimmed.ts
@@ -0,0 +1,28 @@
+/**
+ * @desc
+ *  A tag function trimming the leading and trailing whitespace from multi-line strings.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#Tagged_template_literals
+ *
+ * @param {TemplateStringsArray} templates
+ * @param {Array<string>} placeholders
+ * @returns {string}
+ */
+export function trimmed(templates: TemplateStringsArray, ...placeholders: string[]) {
+
+    const lines = templates
+        .map((template, i) => i < placeholders.length
+            ? [ template, placeholders[i] ]
+            : [ template ])
+        .reduce((acc, tuple) => acc.concat(tuple))
+        .join('')
+        .split('\n');
+
+    const nonEmptyLines = lines
+        .map(line => line.trim())
+        .filter(line => !! line);
+
+    return nonEmptyLines
+        .map(line => line.replace(/\|\s?(.*)$/, '$1\n'))
+        .join('');
+}
