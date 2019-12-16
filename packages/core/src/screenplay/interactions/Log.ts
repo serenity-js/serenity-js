9ad65e8d6b399f886270aef27beb9c8f71bf753f
Jan Molak <jan.molak@smartcodeltd.co.uk>
2019-12-16 00:08:19 +0000

    docs(core): documented the interaction to Log

--- packages/core/src/screenplay/interactions/Log.ts
+++ packages/core/src/screenplay/interactions/Log.ts
@@ -5,18 +5,55 @@ import { AnswersQuestions, CollectsArtifacts, UsesAbilities } from '../actor';
 import { Answerable } from '../Answerable';
 import { Interaction } from '../Interaction';
 
+/**
+ * @desc
+ *  Enables the {@link Actor} to log arbitrary static values and answers to {@link Question}s,
+ *  so that they can be printed to the terminal by the [`ConsoleReporter`](/modules/console-reporter/)
+ *  and attached to the HTML report by the [`SerenityBDDReporter`](/modules/serenity-bdd/).
+ *
+ * @example
+ *  import { Log } from '@serenity-js/core';
+ *  import { Website } from '@serenity-js/protractor';
+ *
+ *  actor.attemptsTo(
+ *      Log.the('Current page', Website.title(), Website.url()),
+ *  );
+ *
+ * @extends {Interaction}
+ */
 export class Log extends Interaction {
+
+    /**
+     * @desc
+     *  Instantiates a new {@link Log} {@link Interaction}.
+     *
+     * @param {...items: any[]} items
+     *  The items to be logged
+     * @returns {Interaction}
+     */
     static the(...items: Array<Answerable<any>>) {
         return new Log(items);
     }
 
+    /**
+     * @param {...items: any[]} items
+     *  The items to be logged
+     */
     constructor(
         private readonly items: Array<Answerable<any>>,
     ) {
         super();
     }
 
-    performAs(actor: UsesAbilities & AnswersQuestions & CollectsArtifacts): PromiseLike<void> {
+    /**
+     * @desc
+     *  Makes the provided {@link Actor}
+     *  perform this {@link Interaction}.
+     *
+     * @param {UsesAbilities & CollectsArtifacts & AnswersQuestions} actor
+     * @returns {Promise<void>}
+     */
+    performAs(actor: UsesAbilities & AnswersQuestions & CollectsArtifacts): Promise<void> {
         return Promise.all(
                 this.items.map(item => actor.answer(item)),
             )
@@ -27,6 +64,13 @@ export class Log extends Interaction {
             );
     }
 
+    /**
+     * @desc
+     *  Generates a description of this interaction
+     *
+     * @returns {string}
+     *  Description of the {@link Interaction}
+     */
     toString() {
         return `#actor logs: ${ this.items.join(', ') }`;
     }
