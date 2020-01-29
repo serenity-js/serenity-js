d5382bf36ea0da2efb740f6fbbf3a98299cbb8d0
Jan Molak <jan.molak@smartcodeltd.co.uk>
2020-01-29 17:00:48 +0000

    fix(jasmine): make Jasmine report the error stack trace correctly
    Internal Jasmine stack trace parser removes the error message of any nested errors (probably because
    Jasmine never had the requirement to support nested errors), so instead of using that and in absence
    of a configuration mechanism we rely on Serenity/JS RuntimeError class to produce the stack

--- packages/jasmine/spec/monkeyPatched.spec.ts
+++ packages/jasmine/spec/monkeyPatched.spec.ts
@@ -1,15 +1,23 @@
 import { expect } from '@integration/testing-tools';
+import { TestCompromisedError } from '@serenity-js/core';
 import * as sinon from 'sinon';
 import serenityReporterForJasmine = require('../src');
 
 describe('@serenity-js/jasmine', () => {
 
     let jasmine: any;
+    let attrs: any;
 
     beforeEach(() => {
         const env = {
             beforeEach: sinon.spy(),
             afterAll: sinon.spy(),
+        };
+
+        attrs = {
+            expectationResultFactory(fnAttrs) {
+                return {};
+            },
         };
 
         jasmine = {
@@ -35,7 +43,7 @@ describe('@serenity-js/jasmine', () => {
                 // detecting the real location is done in Jasmine integration tests
                 serenityReporterForJasmine(jasmine);
 
-                const spec = new jasmine.Spec();
+                const spec = new jasmine.Spec(attrs);
 
                 expect(spec.result.location.path).to.equal('unknown');
                 expect(spec.result.location.line).to.equal(0);
@@ -54,6 +62,33 @@ describe('@serenity-js/jasmine', () => {
 
                 expect(jasmine.Spec.pendingSpecExceptionMessage).to.equal('=> marked Pending');
             });
+
+            /**
+             * @test {bootstrap}
+             * @test {monkeyPatched}
+             */
+            it('has all the static members of the original constructor function', () => {
+
+                jasmine.Spec.pendingSpecExceptionMessage = '=> marked Pending';
+
+                serenityReporterForJasmine(jasmine);
+
+                expect(jasmine.Spec.pendingSpecExceptionMessage).to.equal('=> marked Pending');
+            });
+
+            it('correctly reports the stacktrace of an error', () => {
+                const error = new TestCompromisedError('DB is down', new Error('Connection error'));
+
+                serenityReporterForJasmine(jasmine);
+
+                const spec = new jasmine.Spec(attrs);
+
+                const result = attrs.expectationResultFactory({ passing: false, error });
+
+                const frames = result.stack.split('\n');
+                expect(frames[0]).to.equal('TestCompromisedError: DB is down');
+                expect(frames).to.include('Caused by: Error: Connection error');
+            });
         });
 
         describe('so that the Suite', () => {
@@ -66,11 +101,25 @@ describe('@serenity-js/jasmine', () => {
                 // detecting the real location is done in Jasmine integration tests
                 serenityReporterForJasmine(jasmine);
 
-                const spec = new jasmine.Suite();
+                const suite = new jasmine.Suite(attrs);
 
-                expect(spec.result.location.path).to.equal('unknown');
-                expect(spec.result.location.line).to.equal(0);
-                expect(spec.result.location.column).to.equal(0);
+                expect(suite.result.location.path).to.equal('unknown');
+                expect(suite.result.location.line).to.equal(0);
+                expect(suite.result.location.column).to.equal(0);
+            });
+
+            it('correctly reports the stacktrace of an error', () => {
+                const error = new TestCompromisedError('DB is down', new Error('Connection error'));
+
+                serenityReporterForJasmine(jasmine);
+
+                const suite = new jasmine.Suite(attrs);
+
+                const result = attrs.expectationResultFactory({ passing: false, error });
+
+                const frames = result.stack.split('\n');
+                expect(frames[0]).to.equal('TestCompromisedError: DB is down');
+                expect(frames).to.include('Caused by: Error: Connection error');
             });
         });
     });
