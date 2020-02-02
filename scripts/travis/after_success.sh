d794180c23cd52ea66dba030d8f238f93ab8cb9b
Jan Molak <jan.molak@smartcodeltd.co.uk>
2020-02-02 16:59:39 +0000

    ci(scripts): release 2.0-alpha from master

--- scripts/travis/after_success.sh
+++ scripts/travis/after_success.sh
@@ -8,25 +8,14 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   exit 0
 fi
 
-if [[ $TRAVIS_BRANCH == '2.0' ]]; then
-  echo "Releasing 2.0 alpha"
-  npx lerna publish prerelease --dist-tag next --yes
-  npm run site:publish
-  exit 0;
-fi
-
 if [[ $TRAVIS_BRANCH != 'master' ]]; then
   echo "Builds from a branch are not released to NPM"
   exit 0
 fi
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  npm run semantic-release
-
-  echo "[DEBUG] CHANGELOG"
-  find packages -maxdepth 2 -name 'CHANGELOG.md' -print0 | xargs -0 -I % sh -c 'echo %; cat %'
-
-  cp packages/serenity-js/CHANGELOG.md book/_changelog/serenity-js.md
-
-  npm run coverage:publish
+  echo "Releasing 2.0 alpha"
+  npx lerna publish prerelease --dist-tag next --yes
+  npm run site:publish
+  exit 0;
 fi
