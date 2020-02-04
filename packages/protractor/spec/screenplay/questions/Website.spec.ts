5cbf32e4a7f798b76863b55326d40ecb441a7a4b
Jan Molak <jan.molak@smartcodeltd.co.uk>
2020-02-04 10:42:26 +0000

    test(protractor): make sure that the issue reported by @san-ouadghiri is no longer present in v2
    Closes #273

--- packages/protractor/spec/screenplay/questions/Website.spec.ts
+++ packages/protractor/spec/screenplay/questions/Website.spec.ts
@@ -7,8 +7,8 @@ import { pageFromTemplate } from '../../fixtures';
 describe('Website', () => {
 
     describe('title', () => {
-        /** @test {Attribute} */
-        it('allows the actor to read an attribute of a DOM element', () => actorCalled('Bernie').attemptsTo(
+        /** @test {Website} */
+        it('allows the actor to read the title of the website', () => actorCalled('Bernie').attemptsTo(
             Navigate.to(pageFromTemplate(`
                 <html>
                     <head>
@@ -23,11 +23,21 @@ describe('Website', () => {
 
     describe('url', () => {
 
-        /** @test {Attribute} */
-        it('allows the actor to read an attribute of a DOM element', () => actorCalled('Bernie').attemptsTo(
+        /** @test {Website} */
+        it('allows the actor to read the URL of the website', () => actorCalled('Bernie').attemptsTo(
             Navigate.to(`chrome://accessibility/`),
 
             Ensure.that(Website.url(), equals(`chrome://accessibility/`)),
+        ));
+
+        /**
+         *  @test {Website}
+         *  @see https://github.com/serenity-js/serenity-js/issues/273
+         */
+        it(`correctly represents the URL containing special characters`, () => actorCalled('Bernie').attemptsTo(
+            Navigate.to(`chrome://accessibility/fr/noworries/#`),
+
+            Ensure.that(Website.url(), equals(`chrome://accessibility/fr/noworries/#`)),
         ));
     });
 });
