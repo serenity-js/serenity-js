bfdad0fa4dc0f007b85cc5fb8ef8397a8f46ad57
Jan Molak <jan.molak@smartcodeltd.co.uk>
2020-02-04 00:53:37 +0000

    ci(travis): build the site after producing the changelog so that it's included in the site

--- scripts/travis/after_success.sh
+++ scripts/travis/after_success.sh
@@ -4,18 +4,26 @@ set -e
 npm run coverage:publish
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Building the site to ensure there are no parse errors."
+  make site
+
   echo "Pull requests are not released to NPM."
   exit 0
 fi
 
 if [[ $TRAVIS_BRANCH != 'master' ]]; then
+  echo "Building the site to ensure there are no parse errors."
+  make site
+
   echo "Builds from a branch are not released to NPM"
   exit 0
 fi
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  echo "Releasing 2.0 alpha"
+  echo "Releasing 2.0"
   npx lerna publish --yes
+
+  make site
   npm run site:publish
   exit 0;
 fi
