6c81add4c838b63d46856e6ca6144f0b7c7768f4
Andreas Markussen <andreas@markussen.dk>
2020-04-28 08:46:28 +0200

    fixed two bugs

--- packages/assertions/src/expectations/isTrue.ts
+++ packages/assertions/src/expectations/isTrue.ts
@@ -3,6 +3,6 @@ import { Answerable } from '@serenity-js/core';
 import { Expectation } from '../Expectation';
 
 export function isTrue(): Expectation<boolean> {
-  return Expectation.thatActualShould<boolean, boolean>(`have value that is `, true)
+  return Expectation.thatActualShould<boolean, boolean>(`have value that is`, true)
     .soThat((actualValue, expectedValueAlwaysTrue) => actualValue === expectedValueAlwaysTrue);
 }
