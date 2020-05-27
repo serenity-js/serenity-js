b45053785538d9b7eddf70c11ec3ca7d6c2f6ce8
Andreas Markussen <andreasmarkussen@users.noreply.github.com>
2020-05-27 07:32:51 +0200

    Update isTrue.ts
    removed extra whitespace at last line

--- packages/assertions/src/expectations/isTrue.ts
+++ packages/assertions/src/expectations/isTrue.ts
@@ -4,4 +4,3 @@ export function isTrue() {
   return Expectation.thatActualShould<boolean, boolean>(`have value that is`, true)
   .soThat((actualValue, expectedValueAlwaysTrue) => actualValue === expectedValueAlwaysTrue);
 };
- 
