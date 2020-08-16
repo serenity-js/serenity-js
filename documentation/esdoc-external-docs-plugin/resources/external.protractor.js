97cfe3259d282dd3b6ff21ecaf9027b48b37010a
Jan Molak <jan.molak@smartcodeltd.co.uk>
2020-08-16 14:23:56 +0200

    docs(protractor): enabled linking to external protractor and selenium-webdriver api docs

---
+++ documentation/esdoc-external-docs-plugin/resources/external.protractor.js
@@ -0,0 +1,11 @@
+/**
+ * @external {ElementFinder} https://www.protractortest.org/#/api?view=ElementFinder
+ */
+
+/**
+ * @external {ElementArrayFinder} https://www.protractortest.org/#/api?view=ElementArrayFinder
+ */
+
+/**
+ * @external {ProtractorBrowser} https://www.protractortest.org/#/api?view=ProtractorBrowser
+ */
