dcf6f6103a0c5adf13c5a75a9d2d969973aa5f02
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2020-11-26 10:11:16 +0000

    fix(core): reverted experimental support for extendable abilities
    The idea is sound, but I need to re-think the implementation as there are some edge cases I haven't
    considered

--- packages/core/src/screenplay/abilities/Extension.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @experimental
- */
-export interface Extension<Subject> {
-    applyTo(subject: Subject): Promise<Subject> | Subject;
-}
--- packages/core/src/screenplay/abilities/ExtensionType.ts
+++
@@ -1,6 +0,0 @@
-import { Extension } from './Extension';
-
-/**
- * @experimental
- */
-export type ExtensionType<Subject, ET extends Extension<Subject> = Extension<Subject>> = new (...args: any[]) => ET
