94966b05e90a7d7b97d789f4fc1c92b4900f9fe4
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-01-08 00:57:08 +0000

    docs(examples): updated cucumber-reporting examples to demonstrate Cucumber 7 Rule/Example syntax

--- examples/cucumber-reporting/features/step_definitions/sample.steps.ts
+++ examples/cucumber-reporting/features/step_definitions/sample.steps.ts
@@ -1,41 +1,41 @@
 import { AssertionError, TestCompromisedError } from '@serenity-js/core';
-import { Given, TableDefinition, Then, When } from 'cucumber';
+import { Given, DataTable, Then, When } from '@cucumber/cucumber';
 
-Given(/^.*step.*passes$/, function() {
+Given(/^.*step.*passes$/, function () {
     return Promise.resolve();
 });
 
-Given(/^.*step.*fails with an assertion error$/, function() {
+Given(/^.*step.*fails with an assertion error$/, function () {
     return Promise.reject(new AssertionError('expected true to equal false', false, true));
 });
 
-Given(/^.*step.*fails with a generic error$/, function() {
+Given(/^.*step.*fails with a generic error$/, function () {
     return Promise.reject(new Error(`Something's wrong`));
 });
 
-Given(/^.*step.*fails with an error compromising the test$/, function() {
+Given(/^.*step.*fails with an error compromising the test$/, function () {
     return Promise.reject(new TestCompromisedError(`Something's wrong`));
 });
 
-Given(/^.*step.*marked as pending/, function() {
+Given(/^.*step.*marked as pending/, function () {
     return Promise.resolve('pending');
 });
 
-Given(/^.*step.*receives a table:$/, function(data: TableDefinition) {
+Given(/^.*step.*receives a table:$/, function (data: DataTable) {
     return Promise.resolve();
 });
 
-Given(/^.*step.*receives a doc string:$/, function(docstring: string) {
+Given(/^.*step.*receives a doc string:$/, function (docstring: string) {
     return Promise.resolve();
 });
 
-Given(/^.*step.*times out$/,  { timeout: 100 }, function() {
+Given(/^.*step.*times out$/,  { timeout: 100 }, function () {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, 1000);
     });
 });
 
-When(/^(.*) makes a contribution/, function(developerName: string, dataTable: TableDefinition) {
+When(/^(.*) makes a contribution/, function(developerName: string, dataTable: DataTable) {
     return Promise.resolve();
 });
 
