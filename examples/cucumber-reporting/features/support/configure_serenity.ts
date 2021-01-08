bb4f38006bd1ca564d39b5c528f2e87b502672fe
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-01-08 00:57:08 +0000

    docs(examples): updated cucumber-reporting examples to demonstrate Cucumber 7 Rule/Example syntax

--- examples/cucumber-reporting/features/support/configure_serenity.ts
+++ examples/cucumber-reporting/features/support/configure_serenity.ts
@@ -1,8 +1,8 @@
+import { setDefaultTimeout } from '@cucumber/cucumber';
 import { ConsoleReporter } from '@serenity-js/console-reporter';
-import { ArtifactArchiver, configure } from '@serenity-js/core';
+import { ArtifactArchiver, configure, StreamReporter } from '@serenity-js/core';
 import { SerenityBDDReporter } from '@serenity-js/serenity-bdd';
-
-import { setDefaultTimeout } from 'cucumber';
+import * as fs from 'fs';
 
 setDefaultTimeout(5000);
 
