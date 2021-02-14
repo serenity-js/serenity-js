a07f42c081afb67362545d94dec764b9b4f45eff
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-02-14 03:41:13 +0000

    test(protractor): improved performance on Windows OS

--- packages/protractor/spec/screenplay/interactions/ResizeBrowserWindow.spec.ts
+++ packages/protractor/spec/screenplay/interactions/ResizeBrowserWindow.spec.ts
@@ -15,8 +15,8 @@ describe('ResizeBrowserWindow', () => {
             actorCalled('Nick').attemptsTo(
                 ResizeBrowserWindow.to(640, 480),
                 Ensure.that(Window.size(), equals({width: 640, height: 480})),
-                ResizeBrowserWindow.to(480, 640),
-                Ensure.that(Window.size(), equals({width: 480, height: 640})),
+                ResizeBrowserWindow.to(1024, 768),
+                Ensure.that(Window.size(), equals({width: 1024, height: 768})),
             ));
 
         /** @test {ResizeBrowserWindow.to} */
