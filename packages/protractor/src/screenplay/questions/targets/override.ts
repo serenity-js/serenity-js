db1a2c27d8fad34298162afeb615e1e4bbf56bba
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-02-25 23:23:32 +0000

    chore(deps-dev): upgraded to TypeScript 4.2.2

--- packages/protractor/src/screenplay/questions/targets/override.ts
+++ packages/protractor/src/screenplay/questions/targets/override.ts
@@ -6,7 +6,7 @@
  */
 export function override<T extends object, K extends keyof T>(obj: T, name: K, implementation: T[K]) {
     return new Proxy<T>(obj, {
-        get(o: T, prop: string | number) {
+        get(o: T, prop: string | symbol) {
             return prop === name
                 ? implementation
                 : obj[prop];
