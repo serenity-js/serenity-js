5ef10966f6af88c24a7d27b7123ec4087f0e2d66
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-03-10 12:27:42 +0000

    feat(assertions): re-exported Expectation and Check from core to avoid breaking tests using Serenity/JS 2.24.1 and older

--- packages/assertions/spec/Expectation.spec.ts
+++ packages/assertions/spec/Expectation.spec.ts
@@ -1,9 +1,9 @@
 import 'mocha';
 
 import { expect } from '@integration/testing-tools';
-import { actorCalled, Answerable, AssertionError, Expectation } from '@serenity-js/core';
+import { actorCalled, Answerable, AssertionError } from '@serenity-js/core';
 import { given } from 'mocha-testdata';
-import { and, Ensure, equals, isGreaterThan, isLessThan, or } from '../src';
+import { and, Ensure, Expectation, equals, isGreaterThan, isLessThan, or } from '../src';
 import { isIdenticalTo, p, q } from './fixtures';
 
 /** @test {Expectation} */
