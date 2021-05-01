98d35f1eb73b574c44d90485a52b240383bcb5cd
viper3400 <viper_3400@gmx.de>
2021-05-01 12:00:53 +0200

    feat(assertions): implemented review feedback for containInAnyOrder function

--- packages/assertions/spec/expectations/containInAnyOrder.spec.ts
+++ packages/assertions/spec/expectations/containInAnyOrder.spec.ts
@@ -17,6 +17,55 @@ describe('containInAnyOrder', () => {
     });
 
     /** @test {containInAnyOrder} */
+    it('allows for the actor flow to continue when the "actual" array of numbers contains "expected" in any order', () => {
+        return expect(actorCalled('Simon').attemptsTo(
+            Ensure.that([1, 2, 4, 6, 7],
+                containInAnyOrder([1, 2, 6, 7, 4])),
+        )).to.be.fulfilled;
+    });
+
+    /** @test {containInAnyOrder} */
+    it('allows for the actor flow to continue when the "actual" array of booleans contains "expected" in any order', () => {
+        return expect(actorCalled('Simon').attemptsTo(
+            Ensure.that([false, false, true, true, true],
+                containInAnyOrder([true, false, true, false, true])),
+        )).to.be.fulfilled;
+    });
+
+    /** @test {containInAnyOrder} */
+    it('allows for the actor flow to continue when the "actual" array of objects contains "expected" in any order', () => {
+        return expect(actorCalled('Simon').attemptsTo(
+            Ensure.that([{id: 1, name: 'butter'}, {id: 2, name: 'eggs'}],
+                containInAnyOrder([{id: 2, name: 'eggs'}, {id: 1, name: 'butter'}], 'id')),
+        )).to.be.fulfilled;
+    });
+
+    /** @test {containInAnyOrder} */
+    it('allows for the actor flow to continue when the "actual" array of objects contains "expected" in any order', () => {
+        return expect(actorCalled('Simon').attemptsTo(
+            Ensure.that([{id: 1, custom: 8, name: 'butter'}, {id: 2, name: 'eggs'}],
+                containInAnyOrder([{id: 2, name: 'eggs'}, {id: 1, name: 'butter', custom: 8}], 'id')),
+        )).to.be.fulfilled;
+    });
+
+    /** @test {containInAnyOrder} */
+    it('allows for the actor flow to continue when the "actual" array of objects contains "expected" in any order with a sort key', () => {
+        return expect(actorCalled('Simon').attemptsTo(
+            Ensure.that([{id: 1, custom: 8, name: 'butter'}, {id: 2, name: 'eggs'}],
+                containInAnyOrder([{id: 2, name: 'eggs'}, {id: 1, name: 'butter', custom: 8}], 'name')),
+        )).to.be.fulfilled;
+    });
+
+
+    /** @test {containInAnyOrder} */
+    it('breaks the actor flow to continue when the "actual" array of objects is missing the "expected"', () => {
+        return expect(actorCalled('Simon').attemptsTo(
+            Ensure.that([{name: 'butter', id: 1}, {id: 2, name: 'eggs'}],
+                containInAnyOrder([{id: 2, name: 'ham'}, {id: 1, name: 'butter'}], 'id')),
+        )).to.be.rejected;
+    });
+
+    /** @test {containInAnyOrder} */
     it('breaks the actor flow when "actual" is an empty list', () => {
         return expect(actorCalled('Simon').attemptsTo(
             Ensure.that([], containInAnyOrder(expected)),
@@ -41,8 +90,8 @@ describe('containInAnyOrder', () => {
     /** @test {containInAnyOrder} */
     it('contributes to a human-readable description', () => {
         const actual = () => Question.about('list of items', actor => expected);
-
         expect(Ensure.that(actual(), containInAnyOrder(expected)).toString())
             .to.equal(`#actor ensures that list of items does contain all items in any order from [ 'butter', 'butter', 'eggs', 'milk', 'sugar' ]`);
     });
 });
+
--- packages/assertions/src/expectations/containInAnyOrder.ts
+++ packages/assertions/src/expectations/containInAnyOrder.ts
@@ -1,7 +1,16 @@
 import { Answerable, Expectation } from '@serenity-js/core';
+import { AssertionError } from 'chai';
 import { equal } from 'tiny-types/lib/objects'; // tslint:disable-line:no-submodule-imports
 
-export function containInAnyOrder(expected: Answerable<string[]>): Expectation<string[]> {
-    return Expectation.thatActualShould<string[], string[]>('contain all items in any order from', expected)
-        .soThat((actualValue, expectedValue) => equal(actualValue.sort(), expectedValue.sort()));
+export function containInAnyOrder(expected: Answerable<any[]>, sortByProperty = ''): Expectation<any[]> {
+    if (typeof(expected[0]) !== 'object') {
+        return Expectation.thatActualShould<any[], any[]>('contain all items in any order from', expected)
+            .soThat((actualValue, expectedValue) => equal(actualValue.sort(), expectedValue.sort()));
+    } else {
+        if (sortByProperty === '') throw new AssertionError('Expected as sortByProperty when comparing objects.')
+        return Expectation.thatActualShould<any[], any[]>(`contain all items in any order sorted by '${sortByProperty}' from`, expected)
+            .soThat((actualValue, expectedValue) => equal(
+                actualValue.sort((a, b) => (a[sortByProperty] > b[sortByProperty] ? -1 : 1)),
+                expectedValue.sort((a, b) => (a[sortByProperty] > b[sortByProperty] ? -1 : 1))))
+    }
 }
