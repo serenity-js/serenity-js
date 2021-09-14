f60abb7d33be57d5346b67b280dc256d156ff934
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-09-14 23:26:40 +0100

    test(protractor): adjusted the size of the browser window used in internal Serenity/JS tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -20,7 +20,7 @@ const localChromeConfig = {
                 '--disable-infobars',
                 '--headless',
                 '--disable-gpu',
-                '--window-size=200x100',
+                '--window-size=1024x768',
             ]
         },
     }
