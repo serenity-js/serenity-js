90c17dc59d569742e304f37b28b61cd739382a0a
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-09-21 00:56:17 +0100

    style(web): corrected formatting

--- integration/web/spec/screenplay/questions/LastScriptExecution.spec.ts
+++ integration/web/spec/screenplay/questions/LastScriptExecution.spec.ts
@@ -17,32 +17,34 @@ describe('LastScriptExecution', function () {
         /** @test {ExecuteScript.sync} */
         /** @test {ExecuteSynchronousScript} */
         /** @test {LastScriptExecution} */
-        it('allows the actor to retrieve the result of the script execution', () => actorCalled('Joe').attemptsTo(
-            Navigate.to('/screenplay/questions/last-script-execution/result.html'),
+        it('allows the actor to retrieve the result of the script execution', () =>
+            actorCalled('Joe').attemptsTo(
+                Navigate.to('/screenplay/questions/last-script-execution/result.html'),
 
-            Enter.theValue(actorCalled('Joe').name).into(Sandbox.Input),
+                Enter.theValue(actorCalled('Joe').name).into(Sandbox.Input),
 
-            ExecuteScript.sync(`function() {
-                var field = arguments[0];
-                return field.value;
-            }`).withArguments(Sandbox.Input),
+                ExecuteScript.sync(`function() {
+                    var field = arguments[0];
+                    return field.value;
+                }`).withArguments(Sandbox.Input),
 
-            Ensure.that(LastScriptExecution.result<string>(), equals(actorCalled('Joe').name)),
-        ));
+                Ensure.that(LastScriptExecution.result<string>(), equals(actorCalled('Joe').name)),
+            ));
 
         /** @test {ExecuteScript.sync} */
         /** @test {ExecuteSynchronousScript} */
         /** @test {LastScriptExecution} */
-        it('returns undefined or null if the script did not return any result', () => actorCalled('Joe').attemptsTo(
-            Navigate.to('/screenplay/questions/last-script-execution/result.html'),
+        it('returns undefined or null if the script did not return any result', () =>
+            actorCalled('Joe').attemptsTo(
+                Navigate.to('/screenplay/questions/last-script-execution/result.html'),
 
-            ExecuteScript.sync(`
-                /* do nothing */
-            `),
+                ExecuteScript.sync(`
+                    /* do nothing */
+                `),
 
-            // Selenium returns `null`, WebdriverIO returns an `undefined`
-            Ensure.that(LastScriptExecution.result<null>(), or(equals(undefined), equals(null))),  // eslint-disable-line unicorn/no-null
-        ));
+                // Selenium returns `null`, WebdriverIO returns an `undefined`
+                Ensure.that(LastScriptExecution.result<null>(), or(equals(undefined), equals(null))),  // eslint-disable-line unicorn/no-null
+            ));
     });
 
     describe('when used with ExecuteScript.async', () => {
@@ -50,41 +52,44 @@ describe('LastScriptExecution', function () {
         /** @test {ExecuteScript.async} */
         /** @test {ExecuteAsynchronousScript} */
         /** @test {LastScriptExecution} */
-        it('allows the actor to retrieve the result of the script execution', () => actorCalled('Joe').attemptsTo(
-            Navigate.to('/screenplay/questions/last-script-execution/result.html'),
+        it('allows the actor to retrieve the result of the script execution', () =>
+            actorCalled('Joe').attemptsTo(
+                Navigate.to('/screenplay/questions/last-script-execution/result.html'),
 
-            Enter.theValue(actorCalled('Joe').name).into(Sandbox.Input),
+                Enter.theValue(actorCalled('Joe').name).into(Sandbox.Input),
 
-            ExecuteScript.async(`
-                var field = arguments[0];
-                var callback = arguments[arguments.length - 1];
-                callback(field.value);
-            `).withArguments(Sandbox.Input),
+                ExecuteScript.async(`
+                    var field = arguments[0];
+                    var callback = arguments[arguments.length - 1];
+                    callback(field.value);
+                `).withArguments(Sandbox.Input),
 
-            Ensure.that(LastScriptExecution.result<string>(), equals(actorCalled('Joe').name)),
-        ));
+                Ensure.that(LastScriptExecution.result<string>(), equals(actorCalled('Joe').name)),
+            ));
 
         /** @test {ExecuteScript.async} */
         /** @test {ExecuteAsynchronousScript} */
         /** @test {LastScriptExecution} */
-        it('returns undefined or null if the script did not return any result', () => actorCalled('Joe').attemptsTo(
-            Navigate.to('/screenplay/questions/last-script-execution/result.html'),
+        it('returns undefined or null if the script did not return any result', () =>
+            actorCalled('Joe').attemptsTo(
+                Navigate.to('/screenplay/questions/last-script-execution/result.html'),
 
-            ExecuteScript.async(`
-                var callback = arguments[arguments.length - 1];
-                callback();
-            `),
+                ExecuteScript.async(`
+                    var callback = arguments[arguments.length - 1];
+                    callback();
+                `),
 
-            // Selenium returns `null`, WebdriverIO returns an `undefined`
-            Ensure.that(LastScriptExecution.result<null>(), or(equals(undefined), equals(null))),  // eslint-disable-line unicorn/no-null
-        ));
+                // Selenium returns `null`, WebdriverIO returns an `undefined`
+                Ensure.that(LastScriptExecution.result<null>(), or(equals(undefined), equals(null))),  // eslint-disable-line unicorn/no-null
+            ));
     });
 
     /** @test {ExecuteAsynchronousScript} */
     /** @test {LastScriptExecution} */
-    it(`complains if the script hasn't been executed yet`, () => expect(actorCalled('Joe').attemptsTo(
-        Navigate.to('/screenplay/questions/last-script-execution/result.html'),
+    it(`complains if the script hasn't been executed yet`, () =>
+        expect(actorCalled('Joe').attemptsTo(
+            Navigate.to('/screenplay/questions/last-script-execution/result.html'),
 
-        Ensure.that(LastScriptExecution.result<string>(), equals(actorCalled('Joe').name)),
-    )).to.be.rejectedWith(LogicError, 'Make sure to execute a script before checking on the result'));
+            Ensure.that(LastScriptExecution.result<string>(), equals(actorCalled('Joe').name)),
+        )).to.be.rejectedWith(LogicError, 'Make sure to execute a script before checking on the result'));
 });
