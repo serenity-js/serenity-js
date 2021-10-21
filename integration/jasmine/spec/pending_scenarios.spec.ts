a591c2dca63515e67ee60c0d4a12fb3107fe27bc
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-10-21 23:05:11 +0100

    ci(github): debugging

--- integration/jasmine/spec/pending_scenarios.spec.ts
+++ integration/jasmine/spec/pending_scenarios.spec.ts
@@ -43,7 +43,7 @@ describe('@serenity-js/jasmine', function () {
                         expect(outcome).to.be.instanceOf(ImplementationPending);
 
                         expect(outcome.error).to.be.instanceof(ImplementationPendingError);
-                        console.log('>>', outcome.error);
+                        console.log('>>', outcome.error);   // todo: remove
                         expect(outcome.error.message).to.equal('Temporarily disabled with xit');
                     })
                 ;
