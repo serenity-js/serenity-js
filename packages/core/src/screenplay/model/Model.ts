19aa5c7e09aacbf4b561d09e3505408e90433fb2
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-11-15 13:03:28 +0000

    style(core): corrected linting errors

--- packages/core/src/screenplay/model/Model.ts
+++ packages/core/src/screenplay/model/Model.ts
@@ -6,10 +6,10 @@ import { Awaited } from './Awaited';
 export type Model<OriginalType> = {
     [Field in keyof Omit<OriginalType, keyof Question<OriginalType>>]:
     // is it a method?
-        OriginalType[Field] extends (...args: infer OriginalParameters) => infer OriginalMethodResult
-            // make the method signature asynchronous, accepting Answerables and returning a Promise
-            ? (...args: { [P in keyof OriginalParameters]: Answerable<OriginalParameters[P]> }) =>  Question<Promise<Awaited<OriginalMethodResult>>> & Interaction & Model<OriginalMethodResult>
-            // is it an object? wrap each field
-            : Question<Promise<Awaited<OriginalType[Field]>>> & Interaction & Model<OriginalType[Field]>
+    OriginalType[Field] extends (...args: infer OriginalParameters) => infer OriginalMethodResult
+    // make the method signature asynchronous, accepting Answerables and returning a Promise
+        ? (...args: { [P in keyof OriginalParameters]: Answerable<OriginalParameters[P]> }) =>  Question<Promise<Awaited<OriginalMethodResult>>> & Interaction & Model<OriginalMethodResult>
+    // is it an object? wrap each field
+        : Question<Promise<Awaited<OriginalType[Field]>>> & Interaction & Model<OriginalType[Field]>
 }
 
