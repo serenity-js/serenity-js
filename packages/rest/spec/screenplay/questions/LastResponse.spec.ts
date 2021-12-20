00c1579f5b61b370e9292d52c8c29f26cb875535
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-12-20 23:52:43 +0100

    fix(rest): corrected LastResponse to wrap .status(), .body(), .header() and .headers() in Adapters
    This will make it easier for you to navigate complex response objects in a type-safe way
    
    Closes #1082

--- packages/rest/spec/screenplay/questions/LastResponse.spec.ts
+++ packages/rest/spec/screenplay/questions/LastResponse.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 
 import { Ensure, equals } from '@serenity-js/assertions';
-import { LogicError } from '@serenity-js/core';
+import { actorCalled, LogicError } from '@serenity-js/core';
 
 import { GetRequest, LastResponse, Send } from '../../../src';
 import { actorUsingAMockedAxiosInstance } from '../../actors';
@@ -14,63 +14,132 @@ describe('LastResponse', () => {
 
     afterEach(() =>  mock.reset());
 
+    interface Product {
+        id: number;
+        name: string;
+    }
+
+    const
+        url     = '/products/2',
+        body    = { id: 2, name: 'apple' },
+        headers = { 'Content-Type': 'application/json;charset=utf-8' };
+
     describe('when asserting on the response to the last HTTP request', () => {
 
-        interface Product {
-            id: number;
-            name: string;
-        }
-
-        const
-            url     = '/products/2',
-            body    = { id: 2, name: 'apple' },
-            headers = { 'Content-Type': 'application/json;charset=utf-8' };
-
-        beforeEach(() =>  mock.onGet(url).reply(200, body, headers));
+        beforeEach(() =>
+            mock.onGet(url).reply(200, body, headers)
+        );
 
         /**
          * @test {LastResponse}
          * @test {LastResponse.status}
          */
-        it('enables access to the response status', () => actor.attemptsTo(
-            Send.a(GetRequest.to(url)),
-            Ensure.that(LastResponse.status(), equals(200)),
-        ));
+        it('enables access to the response status', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(LastResponse.status(), equals(200)),
+            )
+        );
 
         /**
          * @test {LastResponse}
          * @test {LastResponse.body}
          */
-        it('enables access to the response body', () => actor.attemptsTo(
-            Send.a(GetRequest.to(url)),
-            Ensure.that(LastResponse.body<Product>(), equals(body)),
-        ));
+        it('enables access to the response body', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(LastResponse.body<Product>(), equals(body)),
+            )
+        );
 
         /**
          * @test {LastResponse}
          * @test {LastResponse.header}
          */
-        it('enables access to a specific response header', () => actor.attemptsTo(
-            Send.a(GetRequest.to(url)),
-            Ensure.that(LastResponse.header('Content-Type'), equals(headers['Content-Type'])),
-        ));
+        it('enables access to a specific response header', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(LastResponse.header('Content-Type'), equals(headers['Content-Type'])),
+            )
+        );
 
         /**
          * @test {LastResponse}
          * @test {LastResponse.headers}
          */
-        it('enables access to all response headers', () => actor.attemptsTo(
-            Send.a(GetRequest.to(url)),
-            Ensure.that(LastResponse.headers(), equals(headers)),
-        ));
+        it('enables access to all response headers', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(LastResponse.headers(), equals(headers)),
+            )
+        );
     });
 
-    /**
-     * @test {CallAnApi.mapLastResponse}
-     */
-    it('complains if the last response is attempted to be retrieved without making a request first', () =>
-        expect(actor.attemptsTo(
-            Ensure.that(LastResponse.status(), equals(200)),
-        )).to.be.rejectedWith(LogicError, 'Make sure to perform a HTTP API call before checking on the response'),
-    );
+    describe('when working with adapters', () => {
+
+        beforeEach(() =>
+            mock.onGet(url).reply(200, body, headers)
+        );
+
+        /**
+         * @test {LastResponse}
+         * @test {LastResponse.status}
+         */
+        it('provides an adapter around response status', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(LastResponse.status().toFixed(2), equals('200.00')),
+            )
+        );
+
+        /**
+         * @test {LastResponse}
+         * @test {LastResponse.body}
+         */
+        it('provides an adapter around response body', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(LastResponse.body<Product>().name.toLocaleUpperCase(), equals('APPLE')),
+            )
+        );
+
+        /**
+         * @test {LastResponse}
+         * @test {LastResponse.header}
+         */
+        it('provides an adapter around individual response headers', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(
+                    LastResponse.header('Content-Type').slice(0, LastResponse.header('Content-Type').indexOf(';')),
+                    equals('application/json')
+                ),
+            )
+        );
+
+        /**
+         * @test {LastResponse}
+         * @test {LastResponse.headers}
+         */
+        it('provides an adapter around all response headers', () =>
+            actor.attemptsTo(
+                Send.a(GetRequest.to(url)),
+                Ensure.that(LastResponse.headers()['Content-Type'], equals('application/json;charset=utf-8')),
+            )
+        );
+    });
+
+    describe('when handling errors', () => {
+
+        const Babs = actorCalled('Babs');
+
+        /**
+         * @test {LastResponse.status}
+         */
+        it('complains if the last response is attempted to be retrieved without making a request first', () =>
+            expect(Babs.attemptsTo(
+                Ensure.that(LastResponse.status(), equals(200)),
+            )).to.be.rejectedWith(LogicError, 'Make sure to perform a HTTP API call before checking on the response'),
+        );
+    });
 });
