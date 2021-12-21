a0d9323e8ef2ecfb779596da3c431820104f2137
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-12-21 13:36:57 +0100

    refactor(core): more descriptive names for generic types

--- packages/core/src/screenplay/questions/Mappable.ts
+++ packages/core/src/screenplay/questions/Mappable.ts
@@ -1,5 +1,5 @@
 export interface Mappable<Item_Type, Collection_Type extends Mappable<Item_Type, Collection_Type>> {
-    map<Mapped_Type>(
-        mapping: (item: Item_Type, index?: number, collection?: Collection_Type) => Mapped_Type
-    ): Promise<Array<Awaited<Mapped_Type>>> | Array<Mapped_Type>;
+    map<Mapped_Item_Type>(
+        mapping: (item: Item_Type, index?: number, collection?: Collection_Type) => Mapped_Item_Type
+    ): Promise<Array<Awaited<Mapped_Item_Type>>> | Array<Mapped_Item_Type>;
 }
