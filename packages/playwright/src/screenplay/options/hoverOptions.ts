f1e292d7435753bc5cea4a2d50da85ddec92a2ef
Nikita Chursin <chursinn91@gmail.com>
2021-12-21 13:43:09 +0300

    refactor(playwright): removed old unused code

--- packages/playwright/src/screenplay/options/hoverOptions.ts
+++
@@ -1,9 +0,0 @@
-export interface HoverOptions extends BasicOptions {
-    force?: boolean;
-    modifiers?: ('Alt' | 'Control' | 'Meta' | 'Shift')[];
-    position?: {
-        x: number;
-        y: number;
-    };
-    trial?: boolean;
-}
