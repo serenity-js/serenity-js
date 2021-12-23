a7d2fb73e6c685827e81e63d0fc16afd1a247bc5
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-12-23 03:02:34 +0100

    fix(web): simplified the selectors
    #1074

--- packages/web/src/screenplay/models/selectors/ByCssContainingText.ts
+++ packages/web/src/screenplay/models/selectors/ByCssContainingText.ts
@@ -1,9 +1,4 @@
-import { Answerable } from '@serenity-js/core';
-
 import { Selector } from './Selector';
 
 export class ByCssContainingText extends Selector<[ string, string ]> {
-    constructor(selector: Answerable<string>, text: Answerable<string>) {
-        super([ selector, text ]);
-    }
 }
