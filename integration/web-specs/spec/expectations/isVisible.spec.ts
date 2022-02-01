cf84fb072a6b813338b68bb1dec3932ea8709e3e
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-02-01 14:15:48 +0000

    feat(web): isVisible() works with Web elements in Shadow DOM
    This feature also adds experimental support for Shadow DOM to Protractor. To use it, prefix a
    regular CSS selector with `>>>` - for example `By.css('>>>div.info')`. Target state of this feature
    will be for `By.css` to pierce Shadow DOM by default.
    
    Closes #1085

--- integration/web-specs/spec/expectations/isVisible.spec.ts
+++ integration/web-specs/spec/expectations/isVisible.spec.ts
@@ -1,93 +1,124 @@
 import 'mocha';
 
 import { expect } from '@integration/testing-tools';
-import { Ensure, not } from '@serenity-js/assertions';
+import { Ensure, equals, not, startsWith } from '@serenity-js/assertions';
 import { actorCalled } from '@serenity-js/core';
-import { By, isVisible, Navigate, PageElement, Wait } from '@serenity-js/web';
+import { Attribute, By, Hover, isVisible, Navigate, PageElement, Text, Wait } from '@serenity-js/web';
 
 describe('isVisible', function () {
 
-    const Elements = {
-        displayed:      PageElement.located(By.id('displayed')).describedAs('visible element'),
-        notDisplayed:   PageElement.located(By.id('display-none')).describedAs('not displayed element'),
-        nonExistent:    PageElement.located(By.id('does-not-exist')).describedAs('non-existent element'),
-        hidden:         PageElement.located(By.id('visibility-hidden')).describedAs('hidden element'),
-        notInViewport:  PageElement.located(By.id('not-in-viewport')).describedAs('element outside of the browser viewport'),
-        transparent:    PageElement.located(By.id('opacity-zero')).describedAs('a fully transparent element'),
+    describe('when working with standard document structure', () => {
 
-        foreground:     PageElement.located(By.id('foreground')).describedAs('element in the foreground'),
-        background:     PageElement.located(By.id('background')).describedAs('element in the background'),
-    };
+        const Elements = {
+            displayed:      PageElement.located(By.id('displayed')).describedAs('visible element'),
+            notDisplayed:   PageElement.located(By.id('display-none')).describedAs('not displayed element'),
+            nonExistent:    PageElement.located(By.id('does-not-exist')).describedAs('non-existent element'),
+            hidden:         PageElement.located(By.id('visibility-hidden')).describedAs('hidden element'),
+            notInViewport:  PageElement.located(By.id('not-in-viewport')).describedAs('element outside of the browser viewport'),
+            transparent:    PageElement.located(By.id('opacity-zero')).describedAs('a fully transparent element'),
 
-    beforeEach(() =>
-        actorCalled('Wendy').attemptsTo(
-            Navigate.to('/expectations/is-visible/visibility.html'),
-        ));
+            foreground:     PageElement.located(By.id('foreground')).describedAs('element in the foreground'),
+            background:     PageElement.located(By.id('background')).describedAs('element in the background'),
+        };
 
-    describe('resolves to true when the element', () => {
-
-        /** @test {isVisible} */
-        it('is displayed', () =>
+        beforeEach(() =>
             actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.displayed, isVisible()),
+                Navigate.to('/expectations/is-visible/visibility.html'),
             ));
 
+        describe('resolves to true when the element', () => {
+
+            /** @test {isVisible} */
+            it('is displayed', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.displayed, isVisible()),
+                ));
+
+            /** @test {isVisible} */
+            it('is not covered by other elements', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.foreground, isVisible()),
+                ));
+        });
+
+        describe('resolves to false when the element', () => {
+
+            /** @test {isVisible} */
+            it('is not displayed', () =>
+                // expect(
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.notDisplayed, not(isVisible())),
+                ));
+
+            /** @test {isVisible} */
+            it('does not exist', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.nonExistent, not(isVisible())),
+                ));
+
+            /** @test {isVisible} */
+            it('is hidden', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.hidden, not(isVisible())),
+                ));
+
+            /** @test {isVisible} */
+            it('is not in the viewport', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.notInViewport, not(isVisible())),
+                ));
+
+            /** @test {isVisible} */
+            it('is transparent', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.transparent, not(isVisible())),
+                ));
+
+            /** @test {isVisible} */
+            it('is covered by other elements', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.background, not(isVisible())),
+                ));
+        });
+
         /** @test {isVisible} */
-        it('is not covered by other elements', () =>
-            actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.foreground, isVisible()),
-            ));
+        it('contributes to a human-readable description of an assertion', () => {
+            expect(Ensure.that(Elements.displayed, isVisible()).toString())
+                .to.equal(`#actor ensures that visible element does become visible`);
+        });
+
+        /** @test {isVisible} */
+        it('contributes to a human-readable description of a wait', () => {
+            expect(Wait.until(Elements.displayed, isVisible()).toString())
+                .to.equal(`#actor waits up to 5s until visible element does become visible`);
+        });
     });
 
-    describe('resolves to false when the element', () => {
+    describe.only('when working with Shadow DOM', () => {
 
-        /** @test {isVisible} */
-        it('is not displayed', () =>
-            // expect(
+        const Elements = {
+            cvcLabel:       PageElement.located(By.tagName('label')).describedAs('popup label'),
+            infoComponent:  PageElement.located(By.tagName('popup-info')).describedAs('popup info icon'),
+            infoText:       PageElement.located(By.css('>>>popup-info span.info')).describedAs('popup text'),
+        };
+
+        beforeEach(() =>
             actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.notDisplayed, not(isVisible())),
+                Navigate.to('/expectations/is-visible/shadow_dom.html'),
             ));
 
-        /** @test {isVisible} */
-        it('does not exist', () =>
-            actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.nonExistent, not(isVisible())),
-            ));
+        describe('resolves to true when the element', () => {
 
-        /** @test {isVisible} */
-        it('is hidden', () =>
-            actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.hidden, not(isVisible())),
-            ));
+            /** @test {isVisible} */
+            it('is displayed', () =>
+                actorCalled('Wendy').attemptsTo(
+                    Ensure.that(Elements.infoComponent, isVisible()),
+                    Ensure.that(Elements.infoText, not(isVisible())),
 
-        /** @test {isVisible} */
-        it('is not in the viewport', () =>
-            actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.notInViewport, not(isVisible())),
-            ));
-
-        /** @test {isVisible} */
-        it('is transparent', () =>
-            actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.transparent, not(isVisible())),
-            ));
-
-        /** @test {isVisible} */
-        it('is covered by other elements', () =>
-            actorCalled('Wendy').attemptsTo(
-                Ensure.that(Elements.background, not(isVisible())),
-            ));
-    });
-
-    /** @test {isVisible} */
-    it('contributes to a human-readable description of an assertion', () => {
-        expect(Ensure.that(Elements.displayed, isVisible()).toString())
-            .to.equal(`#actor ensures that visible element does become visible`);
-    });
-
-    /** @test {isVisible} */
-    it('contributes to a human-readable description of a wait', () => {
-        expect(Wait.until(Elements.displayed, isVisible()).toString())
-            .to.equal(`#actor waits up to 5s until visible element does become visible`);
+                    Hover.over(Elements.infoComponent),
+                    Ensure.that(Elements.infoText, isVisible()),
+                    Ensure.that(Text.of(Elements.infoText), equals(Attribute.called('data-text').of(Elements.infoComponent))),
+                ));
+        });
     });
 });
--- packages/webdriverio/src/screenplay/models/WebdriverIOPageElement.ts
+++ packages/webdriverio/src/screenplay/models/WebdriverIOPageElement.ts
@@ -105,32 +105,116 @@ export class WebdriverIOPageElement
 
         const browser = await this.browserFor(element);
 
-        /* eslint-disable no-var */
+        /* eslint-disable no-var,unicorn/consistent-function-scoping,unicorn/prevent-abbreviations,@typescript-eslint/ban-ts-comment,unicorn/no-for-loop,prefer-const */
 
         // get element at cx/cy and see if the element we found is our element, and therefore it's visible.
         return browser.execute(
             /* istanbul ignore next */
             function isVisible(element: any) {
-                if (! element.getBoundingClientRect) {
-                    return false;
+
+                if (!element.getBoundingClientRect || !element.scrollIntoView || !element.contains || !element.getClientRects || !document.elementFromPoint) {
+                    return false
                 }
 
-                var
-                    box = element.getBoundingClientRect(),
-                    cx = box.left + box.width / 2,
-                    cy = box.top + box.height / 2,
-                    e = document.elementFromPoint(cx, cy);
+                // Edge before switching to Chromium
+                const isOldEdge = !!window['StyleMedia']
 
-                for (; e; e = e.parentElement) {
-                    if (e === element)
-                        return true;
+                // get overlapping element
+                function getOverlappingElement (elem: HTMLElement, context?: Document) {
+                    context = context || document
+                    const elemDimension = elem.getBoundingClientRect()
+                    const x = elemDimension.left + (elem.clientWidth / 2)
+                    const y = elemDimension.top + (elem.clientHeight / 2)
+                    return context.elementFromPoint(x, y)
                 }
-                return false;
+
+                // get overlapping element rects (currently only the first)
+                // applicable if element's text is multiline.
+                function getOverlappingRects (elem: HTMLElement, context?: Document) {
+                    context = context || document
+                    const elems = []
+
+                    const rects = elem.getClientRects()
+                    // webdriver clicks on center of the first element's rect (line of text), it might change in future
+                    const rect = rects[0]
+                    const x = rect.left + (rect.width / 2)
+                    const y = rect.top + (rect.height / 2)
+                    elems.push(context.elementFromPoint(x, y))
+
+                    return elems
+                }
+
+                // get overlapping elements
+                function getOverlappingElements (elem: HTMLElement, context?: Document) {
+                    return [getOverlappingElement(elem, context)].concat(getOverlappingRects(elem, context))
+                }
+
+                // is a node a descendant of a given node
+                function nodeContains (elem: HTMLElement, otherNode: HTMLElement) {
+                    // Edge doesn't support neither Shadow Dom nor contains if ShadowRoot polyfill is used
+                    if (isOldEdge) {
+                        let tmpElement = otherNode as HTMLElement | ShadowRoot | Element
+                        while (tmpElement) {
+                            if (tmpElement === elem) {
+                                return true
+                            }
+
+                            tmpElement = tmpElement.parentNode as ShadowRoot
+                            // DocumentFragment / ShadowRoot polyfill like ShadyRoot
+                            if (tmpElement && tmpElement.nodeType === 11 && tmpElement.host) {
+                                tmpElement = tmpElement.host
+                            }
+                        }
+                        return false
+                    }
+
+                    return elem.contains(otherNode)
+                }
+
+                // is one of overlapping elements the `elem` or one of its child
+                function isOverlappingElementMatch (elementsFromPoint: HTMLElement[], elem: HTMLElement): boolean {
+                    if (elementsFromPoint.some(function (elementFromPoint) {
+                        return elementFromPoint === elem || nodeContains(elem, elementFromPoint)
+                    })) {
+                        return true
+                    }
+
+                    // shadow root
+                    // filter unique elements with shadowRoot
+                    // @ts-ignore
+                    let elemsWithShadowRoot = [].concat(elementsFromPoint)
+                    elemsWithShadowRoot = elemsWithShadowRoot.filter(function (x: HTMLElement) {
+                        return x && x.shadowRoot && x.shadowRoot.elementFromPoint
+                    })
+
+                    // getOverlappingElements of every element with shadowRoot
+                    let shadowElementsFromPoint: HTMLElement[] = []
+                    for (let i = 0; i < elemsWithShadowRoot.length; ++i) {
+                        let shadowElement = elemsWithShadowRoot[i]
+                        shadowElementsFromPoint = shadowElementsFromPoint.concat(
+                            getOverlappingElements(elem, (shadowElement as HTMLElement).shadowRoot as any) as any
+                        )
+                    }
+                    // remove duplicates and parents
+                    // @ts-ignore
+                    shadowElementsFromPoint = [].concat(shadowElementsFromPoint)
+                    shadowElementsFromPoint = shadowElementsFromPoint.filter(function (x) {
+                        return !elementsFromPoint.includes(x)
+                    })
+
+                    if (shadowElementsFromPoint.length === 0) {
+                        return false
+                    }
+
+                    return isOverlappingElementMatch(shadowElementsFromPoint, elem)
+                }
+
+                return isOverlappingElementMatch(getOverlappingElements(element) as any as HTMLElement[], element)
             },
             element,
         );
 
-        /* eslint-enable no-var */
+        /* eslint-enable no-var,unicorn/consistent-function-scoping,unicorn/prevent-abbreviations,@typescript-eslint/ban-ts-comment,unicorn/no-for-loop,prefer-const */
     }
 
     // based on https://github.com/webdriverio/webdriverio/blob/dec6da76b0e218af935dbf39735ae3491d5edd8c/packages/webdriverio/src/utils/index.ts#L98
