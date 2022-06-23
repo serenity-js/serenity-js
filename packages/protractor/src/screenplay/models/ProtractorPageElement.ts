12b66d88de2b2f54c5983633910a2eb89822aadc
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-06-23 23:13:11 +0100

    fix(protractor): isVisible check for ProtractorPageElement returns false for non-existent elements
    This change brings behaviour of ProtractorPageElement in line with WebdriverIOPageElement and
    PlaywrightPageElement
    
    re #1236

--- packages/protractor/src/screenplay/models/ProtractorPageElement.ts
+++ packages/protractor/src/screenplay/models/ProtractorPageElement.ts
@@ -201,137 +201,147 @@ export class ProtractorPageElement extends PageElement<ElementFinder> {
      * @see {@link @serenity-js/web/lib/screenplay/models~PageElement}
      */
     async isVisible(): Promise<boolean> {
-        const element: ElementFinder = await this.nativeElement();
+        try {
+            const element: ElementFinder = await this.nativeElement();
 
-        if (!await element.isDisplayed()) {
-            return false;
-        }
-
-        const webElement: WebElement = await element.getWebElement();
-
-        // get element at cx/cy and see if the element we found is our element, and therefore it's visible.
-        return promised(webElement.getDriver().executeScript(
-            `
-            var element = arguments[0];
-            
-            if (!element.getBoundingClientRect || !element.scrollIntoView || !element.contains || !element.getClientRects || !document.elementFromPoint) {
-                return false
+            if (! await element.isDisplayed()) {
+                return false;
             }
 
-            // Edge before switching to Chromium
-            const isOldEdge = !!window['StyleMedia']
-            // returns true for Chrome and Firefox and false for Safari, Edge and IE
-            const scrollIntoViewFullSupport = !(window.safari || isOldEdge)
+            const webElement: WebElement = await element.getWebElement();
 
-            // get overlapping element
-            function getOverlappingElement (elem, context) {
-                context = context || document
-                const elemDimension = elem.getBoundingClientRect()
-                const x = elemDimension.left + (elem.clientWidth / 2)
-                const y = elemDimension.top + (elem.clientHeight / 2)
-                return context.elementFromPoint(x, y)
-            }
-
-            // get overlapping element rects (currently only the first)
-            // applicable if element's text is multiline.
-            function getOverlappingRects (elem, context) {
-                context = context || document
-                const elems = []
-
-                const rects = elem.getClientRects()
-                // webdriver clicks on center of the first element's rect (line of text), it might change in future
-                const rect = rects[0]
-                const x = rect.left + (rect.width / 2)
-                const y = rect.top + (rect.height / 2)
-                elems.push(context.elementFromPoint(x, y))
-
-                return elems
-            }
-
-            // get overlapping elements
-            function getOverlappingElements (elem, context) {
-                return [getOverlappingElement(elem, context)].concat(getOverlappingRects(elem, context))
-            }
-
-            // is a node a descendant of a given node
-            function nodeContains (elem, otherNode) {
-                // Edge doesn't support neither Shadow Dom nor contains if ShadowRoot polyfill is used
-                if (isOldEdge) {
-                    let tmpElement = otherNode
-                    while (tmpElement) {
-                        if (tmpElement === elem) {
-                            return true
+            // get element at cx/cy and see if the element we found is our element, and therefore it's visible.
+            return promised(webElement.getDriver().executeScript(
+                `
+                var element = arguments[0];
+                
+                if (!element.getBoundingClientRect || !element.scrollIntoView || !element.contains || !element.getClientRects || !document.elementFromPoint) {
+                    return false
+                }
+    
+                // Edge before switching to Chromium
+                const isOldEdge = !!window['StyleMedia']
+                // returns true for Chrome and Firefox and false for Safari, Edge and IE
+                const scrollIntoViewFullSupport = !(window.safari || isOldEdge)
+    
+                // get overlapping element
+                function getOverlappingElement (elem, context) {
+                    context = context || document
+                    const elemDimension = elem.getBoundingClientRect()
+                    const x = elemDimension.left + (elem.clientWidth / 2)
+                    const y = elemDimension.top + (elem.clientHeight / 2)
+                    return context.elementFromPoint(x, y)
+                }
+    
+                // get overlapping element rects (currently only the first)
+                // applicable if element's text is multiline.
+                function getOverlappingRects (elem, context) {
+                    context = context || document
+                    const elems = []
+    
+                    const rects = elem.getClientRects()
+                    // webdriver clicks on center of the first element's rect (line of text), it might change in future
+                    const rect = rects[0]
+                    const x = rect.left + (rect.width / 2)
+                    const y = rect.top + (rect.height / 2)
+                    elems.push(context.elementFromPoint(x, y))
+    
+                    return elems
+                }
+    
+                // get overlapping elements
+                function getOverlappingElements (elem, context) {
+                    return [getOverlappingElement(elem, context)].concat(getOverlappingRects(elem, context))
+                }
+    
+                // is a node a descendant of a given node
+                function nodeContains (elem, otherNode) {
+                    // Edge doesn't support neither Shadow Dom nor contains if ShadowRoot polyfill is used
+                    if (isOldEdge) {
+                        let tmpElement = otherNode
+                        while (tmpElement) {
+                            if (tmpElement === elem) {
+                                return true
+                            }
+    
+                            tmpElement = tmpElement.parentNode
+                            // DocumentFragment / ShadowRoot polyfill like ShadyRoot
+                            if (tmpElement && tmpElement.nodeType === 11 && tmpElement.host) {
+                                tmpElement = tmpElement.host
+                            }
                         }
-
-                        tmpElement = tmpElement.parentNode
-                        // DocumentFragment / ShadowRoot polyfill like ShadyRoot
-                        if (tmpElement && tmpElement.nodeType === 11 && tmpElement.host) {
-                            tmpElement = tmpElement.host
-                        }
+                        return false
                     }
-                    return false
+    
+                    return elem.contains(otherNode)
                 }
+    
+                // is one of overlapping elements the \`elem\` or one of its child
+                function isOverlappingElementMatch (elementsFromPoint, elem) {
+                    if (elementsFromPoint.some(function (elementFromPoint) {
+                        return elementFromPoint === elem || nodeContains(elem, elementFromPoint)
+                    })) {
+                        return true
+                    }
+    
+                    // shadow root
+                    // filter unique elements with shadowRoot
+                    // @ts-ignore
+                    let elemsWithShadowRoot = [].concat(elementsFromPoint)
+                    elemsWithShadowRoot = elemsWithShadowRoot.filter(function (x) {
+                        return x && x.shadowRoot && x.shadowRoot.elementFromPoint
+                    })
+    
+                    // getOverlappingElements of every element with shadowRoot
+                    let shadowElementsFromPoint = []
+                    for (let i = 0; i < elemsWithShadowRoot.length; ++i) {
+                        let shadowElement = elemsWithShadowRoot[i]
+                        shadowElementsFromPoint = shadowElementsFromPoint.concat(
+                            getOverlappingElements(elem, shadowElement.shadowRoot)
+                        )
+                    }
+                    // remove duplicates and parents
+                    // @ts-ignore
+                    shadowElementsFromPoint = [].concat(shadowElementsFromPoint)
+                    shadowElementsFromPoint = shadowElementsFromPoint.filter(function (x) {
+                        return !elementsFromPoint.includes(x)
+                    })
+    
+                    if (shadowElementsFromPoint.length === 0) {
+                        return false
+                    }
+    
+                    return isOverlappingElementMatch(shadowElementsFromPoint, elem)
+                }
+    
+                function isElementInViewport (elem) {
+                    if (!elem.getBoundingClientRect) {
+                        return false
+                    }
+            
+                    const rect = elem.getBoundingClientRect()
+            
+                    const windowHeight = (window.innerHeight || document.documentElement.clientHeight)
+                    const windowWidth = (window.innerWidth || document.documentElement.clientWidth)
+            
+                    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) > 0)
+                    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) > 0)
+            
+                    return (vertInView && horInView)
+                }
+    
+                return isElementInViewport(element) && isOverlappingElementMatch(getOverlappingElements(element), element);
+                `,
+                webElement,
+            ));
+        }
+        catch (error) {
 
-                return elem.contains(otherNode)
+            if (error.name === 'NoSuchElementError') {
+                return false;
             }
 
-            // is one of overlapping elements the \`elem\` or one of its child
-            function isOverlappingElementMatch (elementsFromPoint, elem) {
-                if (elementsFromPoint.some(function (elementFromPoint) {
-                    return elementFromPoint === elem || nodeContains(elem, elementFromPoint)
-                })) {
-                    return true
-                }
-
-                // shadow root
-                // filter unique elements with shadowRoot
-                // @ts-ignore
-                let elemsWithShadowRoot = [].concat(elementsFromPoint)
-                elemsWithShadowRoot = elemsWithShadowRoot.filter(function (x) {
-                    return x && x.shadowRoot && x.shadowRoot.elementFromPoint
-                })
-
-                // getOverlappingElements of every element with shadowRoot
-                let shadowElementsFromPoint = []
-                for (let i = 0; i < elemsWithShadowRoot.length; ++i) {
-                    let shadowElement = elemsWithShadowRoot[i]
-                    shadowElementsFromPoint = shadowElementsFromPoint.concat(
-                        getOverlappingElements(elem, shadowElement.shadowRoot)
-                    )
-                }
-                // remove duplicates and parents
-                // @ts-ignore
-                shadowElementsFromPoint = [].concat(shadowElementsFromPoint)
-                shadowElementsFromPoint = shadowElementsFromPoint.filter(function (x) {
-                    return !elementsFromPoint.includes(x)
-                })
-
-                if (shadowElementsFromPoint.length === 0) {
-                    return false
-                }
-
-                return isOverlappingElementMatch(shadowElementsFromPoint, elem)
-            }
-
-            function isElementInViewport (elem) {
-                if (!elem.getBoundingClientRect) {
-                    return false
-                }
-        
-                const rect = elem.getBoundingClientRect()
-        
-                const windowHeight = (window.innerHeight || document.documentElement.clientHeight)
-                const windowWidth = (window.innerWidth || document.documentElement.clientWidth)
-        
-                const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) > 0)
-                const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) > 0)
-        
-                return (vertInView && horInView)
-            }
-
-            return isElementInViewport(element) && isOverlappingElementMatch(getOverlappingElements(element), element);
-            `,
-            webElement,
-        ));
+            throw error;
+        }
     }
 }
