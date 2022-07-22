c1b38f5cf0d428948067b43efde43144be9eefe0
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-07-22 01:13:04 +0100

    test(core): added tests for the interaction to Log

---
+++ packages/core/spec/screenplay/interactions/Log.spec.ts
@@ -0,0 +1,47 @@
+import { describe, it } from 'mocha';
+import * as sinon from 'sinon';
+
+import { Log, Question, Serenity } from '../../../src';
+import { expect } from '../../expect';
+import { Name } from '../../../src/model';
+
+/** @test {Log} */
+describe('Log', () => {
+
+    const sandbox = sinon.createSandbox();
+
+    let serenity: Serenity;
+
+    beforeEach(async () => {
+        serenity = new Serenity();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    })
+
+    it(`describes what's logged`, () => {
+
+        const log = Log.the('example', 42)
+
+        expect(log.toString()).to.equal(`#actor logs: 'example', 42`);
+    });
+
+    it('emits debugged values so that they can be included in reports', async () => {
+
+        const log = Log.the(Question.about('question adapter', actor => 'some value'));
+
+        const actor = serenity.theActorCalled('Logan');
+
+        const collect = sinon.spy(actor, 'collect');
+
+        await actor.attemptsTo(
+            log,
+        );
+
+        collect.firstCall.args[0].map(decoded => {
+            expect(decoded.data).to.deep.equal(`'some value'`);
+        });
+        expect(collect.firstCall.args[1]).to.equal(new Name(`question adapter`));
+    });
+});
