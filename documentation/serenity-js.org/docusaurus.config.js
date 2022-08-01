5d688f857932ed2fe33fbd70f7b662afa20d0aba
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-01 11:35:30 +0200

    style(website): corrected ESLint rule

--- documentation/serenity-js.org/docusaurus.config.js
+++ documentation/serenity-js.org/docusaurus.config.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-/* eslint disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
