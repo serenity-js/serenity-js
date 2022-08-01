0c6493fdcf15a73e1080b3c581e9053343b6fc48
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-01 23:46:38 +0200

    docs(website): documented @serenity-js/jasmine exported APIs
    re #1197

--- packages/jasmine/src/bootstrap.ts
+++ packages/jasmine/src/bootstrap.ts
@@ -4,17 +4,22 @@ import { monkeyPatched } from './monkeyPatched';
 import { SerenityReporterForJasmine } from './SerenityReporterForJasmine';
 
 /**
- * @desc
- *  Monkey-patches Jasmine's Suite and Spec so that they provide more accurate information and
- *  returns a bootstrapped instance of the {@link SerenityReporterForJasmine} to be registered with Jasmine.
+ * Monkey-patches Jasmine's Suite and Spec so that they provide more accurate information,
+ * and returns a bootstrapped instance of the {@link SerenityReporterForJasmine} to be registered with Jasmine.
  *
- * @example <caption>Registering the reporter from the command line</caption>
+ * ## Registering the reporter from the command line
+ *
+ * ```terminal
  * jasmine --reporter=@serenity-js/jasmine
+ * ```
  *
- * @example <caption>Registering the reporter programmatically</caption>
+ * ## Registering the reporter programmatically
+ *
+ * ```ts
  * import serenityReporterForJasmine = require('@serenity-js/jasmine');
  *
  * jasmine.getEnv().addReporter(serenityReporterForJasmine);
+ * ```
  *
  * @see {@link monkeyPatched}
  * @see {@link SerenityReporterForJasmine}
