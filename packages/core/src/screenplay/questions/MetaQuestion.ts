9e909e8117d728c7dd8b78ca117d41ffd6b0ad79
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-04 01:12:20 +0200

    docs(core): documented Answerables
    RE #1197

--- packages/core/src/screenplay/questions/MetaQuestion.ts
+++ packages/core/src/screenplay/questions/MetaQuestion.ts
@@ -2,39 +2,35 @@ import { Answerable } from '../Answerable';
 import { Question } from '../Question';
 
 /**
- * @desc
- *  A {@link Question} about another {@link Answerable}, used to retrieve a transformed version of the value it holds, or to compose {@link Question}s.
+ * A "meta question" is a {@link Question} about another {@link Answerable},
+ * used to retrieve a transformed version of the value that answerable holds.
  *
- *  For example, the question [`Text.of`](/modules/protractor/class/src/screenplay/questions/text/Text.ts~Text.html)
- *  returns text content of an {@link ElementFinder} returned by [`Target`](/modules/protractor/class/src/screenplay/questions/targets/Target.ts~Target.html),
- *  which is a `Question<ElementFinder>`.
+ * For example, the question [[Text.of]]
+ * returns text content of a {@link PageElement}.
  *
- *  `MetaQuestion`s are typically used when filtering a {@link List}.
+ * {@link MetaQuestion|Meta questions} are typically used when filtering a {@link List}.
  *
- * @see {@link List}
+ * ## Learn more
+ * - {@link List}
  *
- * @public
- * @interface
+ * @group Answerables
  */
 export interface MetaQuestion<Supported_Answerable_Type extends Answerable<any>, Answer> {
-    /**
-     * @desc
-     *  Transforms a given `answerable`
-     *  to another {@link Question}.
-     *
-     * @type {function<T>(another: Answerable): Question<Answer>}
-     * @public
-     *
-     * @see {@link List}
-     */
-    of: (answerable: Supported_Answerable_Type) => Question<Answer>;
 
     /**
-     * @desc
-     *  Human-readable description of a given {@link MetaQuestion},
+     * Transforms a given `answerable` to another {@link Question}.
      *
-     * @type {function(): string}
-     * @public
+     * #### Learn more
+     * - {@link List}
      */
-    toString: () => string;
+    of(answerable: Supported_Answerable_Type): Question<Answer>;
+
+    /**
+     * Human-readable description of this {@link MetaQuestion},
+     * typically involving the description of the subject.
+     *
+     * For example, a description of a meta question obout "the text of an element"
+     * would be `text of ${ element.toString() }`
+     */
+    toString(): string;
 }
