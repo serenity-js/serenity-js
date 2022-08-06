e438fbc0111d0f4d3555e686fc4a54f6113c98e6
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-07 00:38:37 +0200

    refactor(cucumber): re-organised the adapter package structure and documented configuration options
    re #1197

--- packages/cucumber/src/adapter/index.ts
+++ packages/cucumber/src/adapter/index.ts
@@ -1,5 +1,3 @@
 /* istanbul ignore file covered in integration tests */
 export * from './CucumberCLIAdapter';
 export * from './CucumberConfig';
-export * from './CucumberFormat';
-export * from './output';
