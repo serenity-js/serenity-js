0241a9d2da0b4b53421c27da7b36d394640c7366
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-08 01:20:25 +0200

    docs(web): documented Web-related errors
    re #1197

--- packages/web/src/errors/BrowserWindowClosedError.ts
+++ packages/web/src/errors/BrowserWindowClosedError.ts
@@ -1,5 +1,15 @@
 import { LogicError } from '@serenity-js/core';
 
+/**
+ * Thrown when you're trying to take a screenshot of a browser window
+ * that's already been closed.
+ *
+ * ## Learn more
+ * - {@link TakeScreenshot}
+ * - [[Page.takeScreenshot]]
+ *
+ * @group Errors
+ */
 export class BrowserWindowClosedError extends LogicError {
     constructor(message: string, cause?: Error) {
         super(message, cause);
--- packages/web/src/errors/ModalDialogObstructsScreenshotError.ts
+++ packages/web/src/errors/ModalDialogObstructsScreenshotError.ts
@@ -1,5 +1,15 @@
 import { LogicError } from '@serenity-js/core';
 
+/**
+ * Thrown when the presence of a JavaScript dialog obstructs taking a screenshot.
+ *
+ * ## Learn more
+ * - {@link Photographer}
+ * - {@link LogicError}
+ * - {@link TakeScreenshot}
+ *
+ * @group Errors
+ */
 export class ModalDialogObstructsScreenshotError extends LogicError {
     constructor(message: string, cause?: Error) {
         super(message, cause);
