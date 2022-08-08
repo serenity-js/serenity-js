da98c5280fe51d60593bdb5a42e9762e5b45b913
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-09 01:15:32 +0200

    docs(web): documented public APIs related to modal dialog handling
    re #1197

--- packages/web/src/screenplay/models/dialogs/DismissedModalDialog.ts
+++ packages/web/src/screenplay/models/dialogs/DismissedModalDialog.ts
@@ -2,6 +2,15 @@ import { ensure, isDefined } from 'tiny-types';
 
 import { ModalDialog } from './ModalDialog';
 
+/**
+ * `DismissedModalDialog` represents a {@link ModalDialog} that has been dismissed
+ * via [[ModalDialog.dismissNext]].
+ *
+ * ## Learn more
+ * - {@link ModalDialog}
+ *
+ * @group Models
+ */
 export class DismissedModalDialog extends ModalDialog {
     constructor(private readonly dialogMessage: string) {
         super();
