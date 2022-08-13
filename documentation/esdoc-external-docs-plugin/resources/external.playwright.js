720a5a9bbdb8917cbb51abeac037233f22f9fd2d
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-13 19:57:51 +0200

    docs(website): removed dependency on esdoc and related modules
    re #1197

--- documentation/esdoc-external-docs-plugin/resources/external.playwright.js
+++
@@ -1,7 +0,0 @@
-/**
- * @external {playwright~Browser} https://playwright.dev/docs/api/class-browser
- */
-
-/**
- * @external {playwright~BrowserContextOptions} https://playwright.dev/docs/api/class-browser#browser-new-context
- */
