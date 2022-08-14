2ab5c86035611c4e3eb42ecfee000786422840f7
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-15 01:08:02 +0200

    test(protractor): simplified several Protractor integration tests

--- integration/protractor-jasmine/spec/screenplay.spec.ts
+++ integration/protractor-jasmine/spec/screenplay.spec.ts
@@ -1,6 +1,6 @@
 import { expect, ifExitCodeIsOtherThan, logOutput, PickEvent } from '@integration/testing-tools';
-import { InteractionStarts, SceneFinished, SceneStarts, SceneTagged, TestRunnerDetected } from '@serenity-js/core/lib/events';
-import { ExecutionSuccessful, FeatureTag, Name } from '@serenity-js/core/lib/model';
+import { AsyncOperationAttempted, AsyncOperationCompleted, InteractionStarts, SceneFinished, SceneFinishes, SceneStarts, SceneTagged, TestRunnerDetected } from '@serenity-js/core/lib/events';
+import { Description, ExecutionSuccessful, FeatureTag, Name } from '@serenity-js/core/lib/model';
 import { describe, it } from 'mocha';
 
 import { protractor } from '../src/protractor';
@@ -20,14 +20,20 @@ describe('@serenity-js/jasmine', function () {
                 expect(result.exitCode).to.equal(0);
 
                 PickEvent.from(result.events)
-                    .next(SceneStarts,         event => expect(event.details.name).to.equal(new Name('A screenplay scenario passes')))
-                    .next(SceneTagged,         event => expect(event.tag).to.equal(new FeatureTag('Jasmine')))
-                    .next(TestRunnerDetected,  event => expect(event.name).to.equal(new Name('Jasmine')))
-                    .next(InteractionStarts,   event => expect(event.details.name).to.equal(new Name(`Jasmine disables synchronisation with Angular`)))
-                    .next(InteractionStarts,   event => expect(event.details.name).to.equal(new Name(`Jasmine navigates to 'chrome://version/'`)))
-                    .next(InteractionStarts,   event => expect(event.details.name).to.equal(new Name(`Jasmine navigates to 'chrome://accessibility/'`)))
-                    .next(InteractionStarts,   event => expect(event.details.name).to.equal(new Name(`Jasmine navigates to 'chrome://chrome-urls/'`)))
-                    .next(SceneFinished,       event => expect(event.outcome).to.equal(new ExecutionSuccessful()))
+                    .next(SceneStarts,              event => expect(event.details.name).to.equal(new Name('A screenplay scenario passes')))
+                    .next(SceneTagged,              event => expect(event.tag).to.equal(new FeatureTag('Jasmine')))
+                    .next(TestRunnerDetected,       event => expect(event.name).to.equal(new Name('Jasmine')))
+                    .next(InteractionStarts,        event => expect(event.details.name).to.equal(new Name(`Jasmine disables synchronisation with Angular`)))
+                    .next(InteractionStarts,        event => expect(event.details.name).to.equal(new Name(`Jasmine navigates to 'chrome://version/'`)))
+                    .next(InteractionStarts,        event => expect(event.details.name).to.equal(new Name(`Jasmine navigates to 'chrome://accessibility/'`)))
+                    .next(InteractionStarts,        event => expect(event.details.name).to.equal(new Name(`Jasmine navigates to 'chrome://chrome-urls/'`)))
+
+                    .next(SceneFinishes,            event => expect(event.details.name).to.equal(new Name('A screenplay scenario passes')))
+                    .next(AsyncOperationAttempted,  event => expect(event.taskDescription).to.equal(new Description('[ProtractorReporter] Invoking ProtractorRunner.afterEach...')))
+                    .next(AsyncOperationCompleted,  event => expect(event.taskDescription).to.equal(new Description('[ProtractorReporter] ProtractorRunner.afterEach succeeded')))
+                    .next(AsyncOperationAttempted,  event => expect(event.taskDescription).to.equal(new Description('[Stage] Dismissing Jasmine...')))
+                    .next(AsyncOperationCompleted,  event => expect(event.taskDescription).to.equal(new Description('[Stage] Dismissed Jasmine successfully')))
+                    .next(SceneFinished,            event => expect(event.outcome).to.equal(new ExecutionSuccessful()))
                 ;
             }));
 });
