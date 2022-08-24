c3b2c3a7b16d73cfb4b1b921536e7c777a1a7336
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-08-24 02:00:12 +0200

    style(website): applied ESLint suggestions
    corrected a broken link

--- documentation/serenity-js.org/src/components/Figure/index.tsx
+++ documentation/serenity-js.org/src/components/Figure/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import useBaseUrl from '@docusaurus/useBaseUrl'
-
 export interface FigureProps {
     imgSrc: string;
     caption: string;
     externalLink: string;
 }
 
-export default function Figure({ imgSrc, caption, externalLink = undefined }: FigureProps) {
-
+export default function Figure({ imgSrc, caption, externalLink }: FigureProps) {
 
     const figcaption = externalLink
         ? <figcaption>{caption} (<a href={ externalLink } target="_blank" rel="noopener noreferrer">source</a>)</figcaption>
