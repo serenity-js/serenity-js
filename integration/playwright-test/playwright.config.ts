411c250be8ec920bf940deb8d692d3ccc0461a72
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-09-14 22:03:03 +0100

    ci(github): corrected test configuration

--- integration/playwright-test/playwright.config.ts
+++ integration/playwright-test/playwright.config.ts
@@ -28,8 +28,8 @@ const config: PlaywrightTestConfig = {
     // fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    /* Disable retries since we're testing how failing tests are reported */
+    retries: 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
