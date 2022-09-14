ca9f04001731904916a76904199d332981957215
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-09-14 21:39:15 +0100

    style(playwright-test): corrected ESLint warning

--- integration/playwright-test/examples/skipped/marked-as-skipped.spec.ts
+++ integration/playwright-test/examples/skipped/marked-as-skipped.spec.ts
@@ -4,6 +4,7 @@ describe('Playwright Test reporting', () => {
 
     describe('A scenario', () => {
 
+        // eslint-disable-next-line mocha/no-skipped-tests
         it.skip('is marked as skipped', () => {
             throw new Error('Playwright should not run this code at all');
         });
