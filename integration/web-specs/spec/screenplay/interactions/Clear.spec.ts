d090458845c23af59561d74421c16160ccc4ff64
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-10-04 17:19:28 +0100

    fix(web): interaction to Clear now supports "contenteditable" elements
    Related tickets: closes #1306

--- integration/web-specs/spec/screenplay/interactions/Clear.spec.ts
+++ integration/web-specs/spec/screenplay/interactions/Clear.spec.ts
@@ -21,6 +21,15 @@ describe('Clear', () => {
             Ensure.that(Value.of(Form.field), equals('')),
         ));
 
+    it('allows the actor to clear the value of an empty "contenteditable" element', () =>
+        actorCalled('Bernie').attemptsTo(
+            Navigate.to('/screenplay/interactions/clear/empty_contenteditable.html'),
+
+            Clear.theValueOf(Form.field),
+
+            Ensure.that(Value.of(Form.field), equals('')),
+        ));
+
     it('does not affect elements with no "value" attribute', () =>
         actorCalled('Bernie').attemptsTo(
             Navigate.to('/screenplay/interactions/clear/input_field_with_no_value.html'),
@@ -68,6 +77,15 @@ describe('Clear', () => {
             Ensure.that(Value.of(Form.field), equals('')),
         ));
 
+    it('allows the actor to clear the value of an "contenteditable" element', () =>
+        actorCalled('Bernie').attemptsTo(
+            Navigate.to('/screenplay/interactions/clear/contenteditable.html'),
+
+            Clear.theValueOf(Form.field),
+
+            Ensure.that(Value.of(Form.field), equals('')),
+        ));
+
     it('complains if the element cannot be cleared', () =>
         expect(
             actorCalled('Bernie').attemptsTo(
@@ -75,7 +93,7 @@ describe('Clear', () => {
 
                 Clear.theValueOf(Form.field),
             )
-        ).to.be.rejectedWith(LogicError, `The input field doesn't seem to have a 'value' attribute that could be cleared.`));
+        ).to.be.rejectedWith(LogicError, `The input field doesn't seem like an element that could be cleared.`));
 
     it('provides a sensible description of the interaction being performed', () => {
         expect(Clear.theValueOf(Form.field).toString())
@@ -87,7 +105,7 @@ describe('Clear', () => {
         const location = activity.instantiationLocation();
 
         expect(location.path.basename()).to.equal('Clear.spec.ts');
-        expect(location.line).to.equal(86);
+        expect(location.line).to.equal(104);
         expect(location.column).to.equal(32);
     });
 });
