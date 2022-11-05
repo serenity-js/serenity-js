0df7411e85419ea8710c81a02f97dc0440adce58
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-11-05 23:55:43 +0000

    docs(website): new outline for the Serenity/JS Handbook
    The new outline will help readers get to writing the tests sooner even before they know the full
    theory behind the framework

--- documentation/serenity-js.org/docs/design/screenplay-pattern.md
+++
@@ -1,5 +0,0 @@
----
-sidebar_position: 1
----
-# The Screenplay Pattern
-
---
+++ documentation/serenity-js.org/src/components/ArticleComingSoon/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Admonition from '@theme/Admonition'
+
+export default function ArticleStubInfo() {
+
+    return (
+        <Admonition type={"info"} title={"Coming soon"}>
+            <p>
+                This article is coming soon! If you'd like us to prioritise writing it - give it a thumbs up 👍
+            </p>
+            <p>
+                To help us secure more time to work on Serenity/JS documentation, please  <a href="https://github.com/sponsors/serenity-js" target="_blank" rel="noopener noreferrer">support our work on GitHub</a>.
+            </p>
+        </Admonition>
+    )
+}
