4a3b4921a7b5203208e63c01fce06b0a26e3d66e
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-11-09 23:02:01 +0100

    docs(website): enabled Giscus comments (#1362)
    * docs(website): enabled Giscus comments
    
    comments should help us gather feedback from the community and make the articles more relevant and
    useful
    
    * docs(website): applied ESLint suggestions

--- documentation/serenity-js.org/src/components/ArticleComingSoon/index.tsx
+++ documentation/serenity-js.org/src/components/ArticleComingSoon/index.tsx
@@ -6,7 +6,7 @@ export default function ArticleStubInfo() {
     return (
         <Admonition type={"info"} title={"Coming soon"}>
             <p>
-                This article is coming soon! If you'd like us to prioritise writing it - give it a thumbs up 👍
+                This article is coming soon! If you'd like us to prioritise writing it - give it a thumbs up in the reactions section below 👍
             </p>
             <p>
                 To help us secure more time to work on Serenity/JS documentation, please  <a href="https://github.com/sponsors/serenity-js" target="_blank" rel="noopener noreferrer">support our work on GitHub</a>.
---
+++ documentation/serenity-js.org/src/theme/DocItem/Layout/index.tsx
@@ -0,0 +1,77 @@
+import { useColorMode,useWindowSize } from '@docusaurus/theme-common';
+import { useDoc } from '@docusaurus/theme-common/internal';
+import Giscus from '@giscus/react';
+import DocBreadcrumbs from '@theme/DocBreadcrumbs';
+import DocItemContent from '@theme/DocItem/Content';
+import DocItemFooter from '@theme/DocItem/Footer';
+import DocItemPaginator from '@theme/DocItem/Paginator';
+import DocItemTOCDesktop from '@theme/DocItem/TOC/Desktop';
+import DocItemTOCMobile from '@theme/DocItem/TOC/Mobile';
+import DocVersionBadge from '@theme/DocVersionBadge';
+import DocVersionBanner from '@theme/DocVersionBanner';
+import clsx from 'clsx';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+/**
+ * Decide if the toc should be rendered, on mobile or desktop viewports
+ */
+function useDocTOC() {
+    const {
+        frontMatter,
+        toc,
+    } = useDoc();
+    const windowSize = useWindowSize();
+    const hidden = frontMatter.hide_table_of_contents;
+    const canRender = !hidden && toc.length > 0;
+    const mobile = canRender ? <DocItemTOCMobile/> : undefined;
+    const desktop = canRender && (windowSize === 'desktop' || windowSize === 'ssr') ? (
+        <DocItemTOCDesktop/>
+    ) : undefined;
+    return {
+        hidden,
+        mobile,
+        desktop,
+    };
+}
+
+export default function DocItemLayout({ children }) {
+    const docTOC = useDocTOC();
+    const { colorMode } = useColorMode();
+    return (
+        <div className="row">
+            <div className={clsx('col', !docTOC.hidden && styles.docItemCol)}>
+                <DocVersionBanner/>
+                <div className={styles.docItemContainer}>
+                    <article>
+                        <DocBreadcrumbs/>
+                        <DocVersionBadge/>
+                        {docTOC.mobile}
+                        <DocItemContent>{children}</DocItemContent>
+                        <DocItemFooter/>
+                    </article>
+                    <DocItemPaginator/>
+
+                    <Giscus
+                        id="giscus-comments"
+                        repo="serenity-js/serenity-js"
+                        repoId="MDEwOlJlcG9zaXRvcnk2MTMwODcxNA=="
+                        category="Comments"
+                        categoryId="DIC_kwDOA6d_Ks4CSdtl"
+                        mapping="pathname"
+                        reactionsEnabled="1"
+                        emitMetadata="0"
+                        inputPosition="top"
+                        theme={colorMode}
+                        lang="en"
+                        strict="1"
+                        loading="lazy"
+                    />
+                </div>
+            </div>
+
+            {docTOC.desktop && <div className="col col--3">{docTOC.desktop}</div>}
+        </div>
+    );
+}
