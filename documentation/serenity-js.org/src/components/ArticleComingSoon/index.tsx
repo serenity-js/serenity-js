51a6e531ab7b10015055c4560b8ac3e6640cc30d
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2022-11-13 00:01:11 +0000

    docs(website): improved article coming soon note

--- documentation/serenity-js.org/src/components/ArticleComingSoon/index.tsx
+++ documentation/serenity-js.org/src/components/ArticleComingSoon/index.tsx
@@ -9,6 +9,9 @@ export default function ArticleStubInfo() {
                 This article is coming soon! If you'd like us to prioritise writing it - give it a thumbs up in the reactions section below 👍
             </p>
             <p>
+                Make sure to follow Serenity/JS on <a href="https://www.linkedin.com/company/serenity-js/" title="Follow Serenity/JS on LinkedIn">LinkedIn</a> or <a href={"https://twitter.com/SerenityJS"} title="Follow Serenity/JS on Twitter">Twitter</a> to get notified when new articles are available 🔔
+            </p>
+            <p>
                 To help us secure more time to work on Serenity/JS documentation, please  <a href="https://github.com/sponsors/serenity-js" target="_blank" rel="noopener noreferrer">support our work on GitHub</a>.
             </p>
         </Admonition>
