b8c9fee66be2dee6235ce4bda3ed8e46731ad553
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-02-04 13:12:50 +0000

    refactor(core): optimised exports in core/errors package

--- packages/core/src/errors/index.ts
+++ packages/core/src/errors/index.ts
@@ -1,12 +1,9 @@
-import { AssertionError } from './model/AssertionError';
-import { ConfigurationError } from './model/ConfigurationError';
+/* eslint-disable simple-import-sort/imports */
+import { AssertionError, ConfigurationError, ImplementationPendingError, LogicError, TestCompromisedError, TimeoutExpiredError, UnknownError } from './model';
 import { ErrorSerialiser } from './ErrorSerialiser';
-import { ImplementationPendingError } from './model/ImplementationPendingError';
-import { LogicError } from './model/LogicError';
-import { TestCompromisedError } from './model/TestCompromisedError';
-import { TimeoutExpiredError } from './model/TimeoutExpiredError';
-import { UnknownError } from './model/UnknownError';
+/* eslint-enable simple-import-sort/imports */
 
+// todo: export ErrorSerialiser as an instance to avoid static method calls
 ErrorSerialiser.registerErrorTypes(
     AssertionError,
     ConfigurationError,
@@ -17,18 +14,10 @@ ErrorSerialiser.registerErrorTypes(
     UnknownError,
 );
 
-export { AssertionError } from './model/AssertionError';
-export { ConfigurationError } from './model/ConfigurationError';
-export { ErrorSerialiser } from './ErrorSerialiser';
-export { ImplementationPendingError } from './model/ImplementationPendingError';
-export { ListItemNotFoundError } from './model/ListItemNotFoundError';
-export { LogicError } from './model/LogicError';
-export { RuntimeError } from './model/RuntimeError';
-export { TestCompromisedError } from './model/TestCompromisedError';
-export { TimeoutExpiredError } from './model/TimeoutExpiredError';
-export { UnknownError } from './model/UnknownError';
+export * from './model';
 
 /* eslint-disable simple-import-sort/exports */
+export { ErrorSerialiser } from './ErrorSerialiser';
 export { ErrorStackParser } from './ErrorStackParser';
 export { ErrorOptions } from './ErrorOptions';
 export { ErrorFactory } from './ErrorFactory';
