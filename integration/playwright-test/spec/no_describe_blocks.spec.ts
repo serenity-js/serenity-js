e6313e47a2e8db537495d1a9b95761996cb8b7c9
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-02-09 00:19:03 +0000

    test(playwright-test): made the test independent of the file system structure
    Looks like Playwright Test grep works slightly differently on Mac than on Linux. This change avoids
    this issue
    
    Related tickets: re #1495

--- integration/playwright-test/spec/no_describe_blocks.spec.ts
+++ integration/playwright-test/spec/no_describe_blocks.spec.ts
@@ -7,7 +7,7 @@ import { playwrightTest } from '../src/playwright-test';
 
 describe('@serenity-js/playwright-test', function () {
 
-    it('uses file name as feature name when no describe blocks are used', () => playwrightTest('--project=default', '--grep=has no describe blocks')
+    it('uses file name as feature name when no describe blocks are used', () => playwrightTest('--project=default', 'no-describe-blocks.spec.ts')
         .then(ifExitCodeIsOtherThan(0, logOutput))
         .then(result => {
 
@@ -20,6 +20,20 @@ describe('@serenity-js/playwright-test', function () {
                 })
                 .next(SceneTagged,         event => expect(event.tag).to.equal(new FeatureTag('no-describe-blocks.spec.ts')))
                 .next(TestRunnerDetected,  event => expect(event.name).to.equal(new Name('Playwright')))
+
+                .next(TestRunFinishes,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
+                .next(TestRunFinished,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
+            ;
+        }));
+
+    it('uses file name as feature name when no describe blocks are used', () => playwrightTest('--project=default', 'nested/no-describe-blocks.spec.ts')
+        .then(ifExitCodeIsOtherThan(0, logOutput))
+        .then(result => {
+
+            expect(result.exitCode).to.equal(0);
+
+            PickEvent.from(result.events)
+                .next(TestRunStarts,       event => expect(event.timestamp).to.be.instanceof(Timestamp))
 
                 .next(SceneStarts,         event => {
                     expect(event.details.name).to.equal(new Name('has no describe blocks'));
