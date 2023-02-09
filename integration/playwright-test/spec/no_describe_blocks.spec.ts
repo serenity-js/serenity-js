7350c3391a274b2077391dd252706f6ab609fba8
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-02-09 00:49:55 +0000

    style(eslint): corrected linting error
    Related tickets: re #1495

--- integration/playwright-test/spec/no_describe_blocks.spec.ts
+++ integration/playwright-test/spec/no_describe_blocks.spec.ts
@@ -7,42 +7,45 @@ import { playwrightTest } from '../src/playwright-test';
 
 describe('@serenity-js/playwright-test', function () {
 
-    it('uses file name as feature name when no describe blocks are used', () => playwrightTest('--project=default', 'no-describe-blocks.spec.ts')
-        .then(ifExitCodeIsOtherThan(0, logOutput))
-        .then(result => {
+    describe('when no describe blocks are used', () => {
 
-            expect(result.exitCode).to.equal(0);
+        it('uses file name as feature name', () => playwrightTest('--project=default', 'no-describe-blocks.spec.ts')
+            .then(ifExitCodeIsOtherThan(0, logOutput))
+            .then(result => {
 
-            PickEvent.from(result.events)
-                .next(TestRunStarts,       event => expect(event.timestamp).to.be.instanceof(Timestamp))
-                .next(SceneStarts,         event => {
-                    expect(event.details.name).to.equal(new Name('has no describe blocks'));
-                })
-                .next(SceneTagged,         event => expect(event.tag).to.equal(new FeatureTag('no-describe-blocks.spec.ts')))
-                .next(TestRunnerDetected,  event => expect(event.name).to.equal(new Name('Playwright')))
+                expect(result.exitCode).to.equal(0);
 
-                .next(TestRunFinishes,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
-                .next(TestRunFinished,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
-            ;
-        }));
+                PickEvent.from(result.events)
+                    .next(TestRunStarts,       event => expect(event.timestamp).to.be.instanceof(Timestamp))
+                    .next(SceneStarts,         event => {
+                        expect(event.details.name).to.equal(new Name('has no describe blocks'));
+                    })
+                    .next(SceneTagged,         event => expect(event.tag).to.equal(new FeatureTag('no-describe-blocks.spec.ts')))
+                    .next(TestRunnerDetected,  event => expect(event.name).to.equal(new Name('Playwright')))
 
-    it('uses file name as feature name when no describe blocks are used', () => playwrightTest('--project=default', 'nested/no-describe-blocks.spec.ts')
-        .then(ifExitCodeIsOtherThan(0, logOutput))
-        .then(result => {
+                    .next(TestRunFinishes,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
+                    .next(TestRunFinished,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
+                ;
+            }));
 
-            expect(result.exitCode).to.equal(0);
+        it('uses nested file name as feature name', () => playwrightTest('--project=default', 'nested/no-describe-blocks.spec.ts')
+            .then(ifExitCodeIsOtherThan(0, logOutput))
+            .then(result => {
 
-            PickEvent.from(result.events)
-                .next(TestRunStarts,       event => expect(event.timestamp).to.be.instanceof(Timestamp))
+                expect(result.exitCode).to.equal(0);
 
-                .next(SceneStarts,         event => {
-                    expect(event.details.name).to.equal(new Name('has no describe blocks'));
-                })
-                .next(SceneTagged,         event => expect(event.tag).to.equal(new FeatureTag('nested/no-describe-blocks.spec.ts')))
-                .next(TestRunnerDetected,  event => expect(event.name).to.equal(new Name('Playwright')))
+                PickEvent.from(result.events)
+                    .next(TestRunStarts,       event => expect(event.timestamp).to.be.instanceof(Timestamp))
 
-                .next(TestRunFinishes,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
-                .next(TestRunFinished,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
-            ;
-        }));
+                    .next(SceneStarts,         event => {
+                        expect(event.details.name).to.equal(new Name('has no describe blocks'));
+                    })
+                    .next(SceneTagged,         event => expect(event.tag).to.equal(new FeatureTag('nested/no-describe-blocks.spec.ts')))
+                    .next(TestRunnerDetected,  event => expect(event.name).to.equal(new Name('Playwright')))
+
+                    .next(TestRunFinishes,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
+                    .next(TestRunFinished,     event => expect(event.timestamp).to.be.instanceof(Timestamp))
+                ;
+            }));
+    });
 });
