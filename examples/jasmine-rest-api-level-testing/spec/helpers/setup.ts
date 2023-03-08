d5a3932b8b37ee9be611fc4938d5cb1f8d26b2de
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-03-08 01:28:58 +0000

    docs(examples): updated jasmine examples to use the latest v3 APIs

--- examples/jasmine-rest-api-level-testing/spec/helpers/setup.ts
+++ examples/jasmine-rest-api-level-testing/spec/helpers/setup.ts
@@ -1,12 +1,16 @@
+import 'jasmine';
+
 import { ConsoleReporter } from '@serenity-js/console-reporter';
 import { ArtifactArchiver, configure } from '@serenity-js/core';
 import { SerenityBDDReporter } from '@serenity-js/serenity-bdd';
 
-configure({
-    crew: [
-        ArtifactArchiver.storingArtifactsAt(__dirname, '../../target/site/serenity'),
-        new SerenityBDDReporter(),
-        ConsoleReporter.forDarkTerminals(),
-    ],
+// eslint-disable-next-line mocha/no-top-level-hooks
+beforeAll(() => {
+    configure({
+        crew: [
+            ArtifactArchiver.storingArtifactsAt(__dirname, '../../target/site/serenity'),
+            new SerenityBDDReporter(),
+            ConsoleReporter.forDarkTerminals(),
+        ],
+    });
 });
-
