f0b295f289f956cf6f2f37d40edd2728671934df
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-03-20 10:17:59 +0000

    docs(website): created placeholders for the articles that are coming soon

--- documentation/serenity-js.org/src/components/ArticleComingSoon/index.tsx
+++ documentation/serenity-js.org/src/components/ArticleComingSoon/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Admonition from '@theme/Admonition'
+import React from 'react';
+import Admonition from '@theme/Admonition';
 
 export default function ArticleStubInfo() {
 
     return (
-        <Admonition type={"info"} title={"Coming soon"}>
-            <p>
-                This article is coming soon! If you'd like us to prioritise writing it - give it a thumbs up in the reactions section below 👍
-            </p>
-            <p>
-                Make sure to follow Serenity/JS on <a href="https://www.linkedin.com/company/serenity-js/" title="Follow Serenity/JS on LinkedIn">LinkedIn</a> or <a href={"https://twitter.com/SerenityJS"} title="Follow Serenity/JS on Twitter">Twitter</a> to get notified when new articles are available 🔔
-            </p>
-            <p>
-                To help us secure more time to work on Serenity/JS documentation, please  <a href="https://github.com/sponsors/serenity-js" target="_blank" rel="noopener noreferrer">support our work on GitHub</a>.
-            </p>
-        </Admonition>
-    )
+        <>
+            <hr/>
+            <Admonition type={ 'info' } title={ 'Coming soon' }>
+
+                <p>
+                    This article is coming soon! If you'd like me to prioritise writing it, give it a thumbs up 👍 in the reactions section below so that I know that this topic is in demand.
+                    Also make sure to follow Serenity/JS on <a href="https://www.linkedin.com/company/serenity-js/" title="Follow Serenity/JS on LinkedIn">LinkedIn</a> or <a
+                    href={ 'https://twitter.com/SerenityJS' } title="Follow Serenity/JS on Twitter">Twitter</a> to get notified 🔔 when new materials and releases are available.
+                </p>
+                <p>
+                    And if you appreciate the amount of effort that goes into maintaining Serenity/JS and producing all the learning materials, please <a href="https://github.com/sponsors/serenity-js"
+                                                                                                                                                          target="_blank" rel="noopener noreferrer">support
+                    our work on GitHub</a>.
+                </p>
+            </Admonition>
+        </>
+    );
 }
