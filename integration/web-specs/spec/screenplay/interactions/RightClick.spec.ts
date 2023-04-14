79878891782ad29a0a117866dd289b170096d61a
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-04-14 16:36:41 +0100

    test(web): code clean-up

--- integration/web-specs/spec/screenplay/interactions/RightClick.spec.ts
+++ integration/web-specs/spec/screenplay/interactions/RightClick.spec.ts
@@ -2,34 +2,30 @@ import 'mocha';
 
 import { expect } from '@integration/testing-tools';
 import { Ensure, equals } from '@serenity-js/assertions';
-import { actorCalled, Wait } from '@serenity-js/core';
-import { Attribute, By, Navigate, PageElement, RightClick, Text } from '@serenity-js/web';
+import { actorCalled, notes, Wait } from '@serenity-js/core';
+import { By, isVisible, Navigate, PageElement, RightClick, Text } from '@serenity-js/web';
 
 describe('RightClick', () => {
 
     const MouseEventLoggerForm = {
-        input:  PageElement.located(By.id('input')).describedAs('input'),
+        input: PageElement.located(By.id('input')).describedAs('input'),
         output: PageElement.located(By.id('output')).describedAs('output'),
     }
 
-    // eslint-disable-next-line unicorn/consistent-function-scoping
-    const json = (text: string): object =>
-        JSON.parse(text);
+    it('allows the actor to right-click on an element', () =>
+        actorCalled('Bernie').attemptsTo(
+            Navigate.to('/screenplay/interactions/right-click/mouse_event_logger.html'),
 
-    it('allows the actor to right-click on an element', () => actorCalled('Bernie').attemptsTo(
-        Navigate.to('/screenplay/interactions/right-click/mouse_event_logger.html'),
+            Wait.until(MouseEventLoggerForm.input, isVisible()),
 
-        Wait.until(Attribute.called('data-event-handled').of(MouseEventLoggerForm.input), equals('false')),
+            RightClick.on(MouseEventLoggerForm.input),
 
-        RightClick.on(MouseEventLoggerForm.input),
+            notes<MyNotes>().set('event', Text.of(MouseEventLoggerForm.output).as(eventDetailsFromJson)),
 
-        Wait.until(Attribute.called('data-event-handled').of(MouseEventLoggerForm.input), equals('true')),
-
-        Ensure.that(Text.of(MouseEventLoggerForm.output).as(json), equals({
-            button: 2,
-            event: 'contextmenu'
-        })),
-    ));
+            Ensure.that(notes<MyNotes>().get('event').event, equals('contextmenu')),
+            Ensure.that(notes<MyNotes>().get('event').button, equals(2)),
+            Ensure.that(notes<MyNotes>().get('event').receiver, equals('input')),
+        ));
 
     it('provides a sensible description of the interaction being performed', () => {
         expect(RightClick.on(MouseEventLoggerForm.input).toString())
@@ -41,7 +37,22 @@ describe('RightClick', () => {
         const location = activity.instantiationLocation();
 
         expect(location.path.basename()).to.equal('RightClick.spec.ts');
-        expect(location.line).to.equal(40);
+        expect(location.line).to.equal(36);
         expect(location.column).to.equal(37);
     });
 });
+
+interface EventDetails {
+    button: number;
+    event: string;
+    receiver: 'input';
+    position: { x: number, y: number };
+}
+
+interface MyNotes {
+    event: EventDetails;
+}
+
+function eventDetailsFromJson(text: string): EventDetails {
+    return JSON.parse(text);
+}
