3411f19a07a372ffa29eac712749b253e19513d6
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-05-04 17:47:52 +0100

    fix(webdriverio): aligned CJS and ESM exports to offer a consistent developer experience

--- packages/webdriverio/src/index.ts
+++ packages/webdriverio/src/index.ts
@@ -1,14 +1,29 @@
 import { serenity } from '@serenity-js/core';
 import { ModuleLoader, Path } from '@serenity-js/core/lib/io/index.js';
+import { Capabilities } from '@wdio/types';
+import { EventEmitter } from 'events';
 
-import { WebdriverIOFrameworkAdapterFactory } from './adapter/index.js';
+import { InitialisesReporters, ProvidesWriteStream } from './adapter/reporter/index.js';
+import { WebdriverIOConfig } from './config/index.js';
 
-const adapterFactory = new WebdriverIOFrameworkAdapterFactory(
-    serenity,
-    new ModuleLoader(process.cwd()),
-    Path.from(process.cwd()),
-);
+export default {
+    async init(
+        cid: string,
+        config: WebdriverIOConfig,
+        specs: string[],
+        capabilities: Capabilities.RemoteCapability,
+        reporter: EventEmitter & ProvidesWriteStream & InitialisesReporters
+    ): Promise<{ hasTests: () => boolean, run: () => Promise<number> }> {
+        const { WebdriverIOFrameworkAdapterFactory } = await import('./adapter/index.js');
 
-export default adapterFactory;
+        const adapterFactory = new WebdriverIOFrameworkAdapterFactory(
+            serenity,
+            new ModuleLoader(process.cwd()),
+            Path.from(process.cwd()),
+        );
+
+        return adapterFactory.init(cid, config, specs, capabilities, reporter);
+    }
+};
 
 export * from './api.js';
