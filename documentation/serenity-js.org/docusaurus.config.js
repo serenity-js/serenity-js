66ddecef6ce2b359361b0e188024cf1f18cd7872
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-06-14 18:03:39 +0100

    docs(website): corrected trailing slash behaviour
    Giscus relies on matching the pathName, so we need to ensure that the trailing slash behaves
    consistently
    
    Related tickets: facebook/docusaurus#3372

--- documentation/serenity-js.org/docusaurus.config.js
+++ documentation/serenity-js.org/docusaurus.config.js
@@ -31,6 +31,7 @@ const config = {
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'throw',
     favicon: 'icons/favicon.ico',
+    trailingSlash: true,
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
