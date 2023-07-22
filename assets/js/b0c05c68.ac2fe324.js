eac7a6d0437baff8618f93875ae46c60d9502f37
jan-molak <1089173+jan-molak@users.noreply.github.com>
2023-07-22 00:23:06 +0000

    Deploying to gh-pages from @ serenity-js/serenity-js@63a09887f5192b79b79e6bcd09b6867713238697 🚀

--- assets/js/b0c05c68.ac2fe324.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_documentation_serenity_js_org=self.webpackChunk_documentation_serenity_js_org||[]).push([[32872],{61746:e=>{e.exports=JSON.parse('{"label":"serenity-bdd","permalink":"/changelog/tags/serenity-bdd","allTagsPath":"/changelog/tags","count":9}')}}]);
