242b6d4f6b6c06a0deb76dff7aba9e5d11302f5d
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-07-25 21:05:08 +0100

    test(playwright-test): improved test coverage of Playwright step reporting

---
+++ integration/playwright-test/examples/screenplay/step-reporting/nested-tasks-with-web-interactions-as-steps.spec.ts
@@ -0,0 +1,35 @@
+import { Answerable, Task } from '@serenity-js/core';
+import { LocalServer, StartLocalServer, StopLocalServer } from '@serenity-js/local-server';
+import { afterEach, describe, it, test } from '@serenity-js/playwright-test';
+import { Navigate } from '@serenity-js/web';
+
+import { ActorsWithLocalServer } from '../actors/ActorsWithLocalServer';
+
+const openWebsiteAt = (url: Answerable<string>) =>
+    Task.where(`#actor opens website at ${ url }`,
+        Navigate.to(url),
+    );
+
+describe('Playwright Test reporting', () => {
+
+    test.use({
+        actors: ({ page, contextOptions }, use) => {
+            use(new ActorsWithLocalServer(page, contextOptions));
+        },
+    });
+
+    describe('A screenplay scenario', () => {
+        it('reports tasks with nested web interactions as Playwright steps', async ({ actor }) => {
+            await actor.attemptsTo(
+                StartLocalServer.onRandomPort(),
+                openWebsiteAt(LocalServer.url()),
+            );
+        });
+
+        afterEach(async ({ actor }) => {
+            await actor.attemptsTo(
+                StopLocalServer.ifRunning(),
+            );
+        });
+    });
+});
