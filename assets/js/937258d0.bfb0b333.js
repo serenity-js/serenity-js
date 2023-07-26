0adb6b2f483044fb630510e16ca6a4b7ed9eef98
jan-molak <1089173+jan-molak@users.noreply.github.com>
2023-07-26 10:10:40 +0000

    Deploying to gh-pages from @ serenity-js/serenity-js@f1132bd533254834f8e907a417d7ea0e0890041f 🚀

---
+++ assets/js/937258d0.bfb0b333.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_documentation_serenity_js_org=self.webpackChunk_documentation_serenity_js_org||[]).push([[77218],{18324:e=>{e.exports=JSON.parse('{"label":"playwright-test","permalink":"/changelog/tags/playwright-test","allTagsPath":"/changelog/tags","count":11}')}}]);
