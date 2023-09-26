900aa8531f992140b4b5aef54da36cddc4953fd6
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-09-26 17:37:34 +0100

    refactor(rest): cleaned up proxy resolution algorithm
    Related tickets: #1949

--- packages/rest/src/screenplay/abilities/AxiosRequestConfigDefaults.ts
+++ packages/rest/src/screenplay/abilities/AxiosRequestConfigDefaults.ts
@@ -1,13 +1,15 @@
 import { type CreateAxiosDefaults } from 'axios';
 
+export interface AxiosRequestConfigProxyDefaults {
+    host: string;
+    port?: number;          // SOCKS proxies don't require port number
+    auth?: {
+        username: string;
+        password: string;
+    };
+    protocol?: string;
+}
+
 export type AxiosRequestConfigDefaults<Data = any> = Omit<CreateAxiosDefaults<Data>, 'proxy'> & {
-    proxy?: {
-        host: string;
-        port?: number;          // SOCKS proxy doesn't require port number
-        auth?: {
-            username: string;
-            password: string;
-        };
-        protocol?: string;
-    }
+    proxy?: AxiosRequestConfigProxyDefaults | false;
 }
