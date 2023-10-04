7c39e2f3153e35a5745e31edbb821d69e32650c0
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-10-04 10:02:24 +0100

    docs(website): downgraded prism-react-renderer

--- documentation/serenity-js.org/docusaurus.config.js
+++ documentation/serenity-js.org/docusaurus.config.js
@@ -4,8 +4,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer').themes.github;
-const darkCodeTheme = require('prism-react-renderer').themes.dracula;
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const path = require('path');
 const redirects = require('./redirects.config');
 const pkg = require('./../../package.json');
