b212741be425b31d094be498be689b978d142aea
jan-molak <1089173+jan-molak@users.noreply.github.com>
2023-12-10 21:15:24 +0000

    Deploying to gh-pages from @ serenity-js/serenity-js@5da27241dd0b4b27e93af703d4b81dd89409c037 🚀

--- assets/js/884ca2ed.73bbff24.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_documentation_serenity_js_org=self.webpackChunk_documentation_serenity_js_org||[]).push([[23912],{48754:e=>{e.exports=JSON.parse('{"label":"protractor","permalink":"/changelog/tags/protractor","allTagsPath":"/changelog/tags","count":11}')}}]);
