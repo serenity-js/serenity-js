29c87f7b85064ad48297c90109d9bb20efeab673
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2024-01-14 13:40:10 +0000

    docs(serenity-bdd): clarified the requirement around file names for using Serenity BDD reporter
    Related tickets: re #1147

--- examples/playwright-test-todomvc/spec/bulk_operations.spec.ts
+++ examples/playwright-test-todomvc/spec/bulk_operations.spec.ts
@@ -7,7 +7,7 @@ import { clearCompletedButton, persistedItemNames, persistedItems, startWithALis
 import { isDisplayedAsCompleted, isDisplayedAsOutstanding, markAsCompleted, markAsOutstanding } from './todo-list-app/TodoItem';
 import { itemCalled, itemNames, items, toggleAllItems } from './todo-list-app/TodoList';
 
-describe('Bulk operations', () => {
+describe.only('Bulk operations', () => {
 
     describe('Todo List App', () => {
 
