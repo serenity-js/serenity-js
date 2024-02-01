9944aeb8a04726bc36124a276cbba66de75e9533
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2024-02-01 00:38:21 +0000

    test(jasmine): updated integration test

--- integration/jasmine/examples/programmatic-reporter.js
+++ integration/jasmine/examples/programmatic-reporter.js
@@ -1,4 +1,4 @@
 const path = require('path');
 const { default: reporter } = require('@serenity-js/jasmine');
 
-jasmine.getEnv().addReporter(reporter(jasmine, { specDirectory: path.resolve(__dirname, '../examples') }));
+jasmine.getEnv().addReporter(reporter({ specDirectory: path.resolve(__dirname, '../examples') }));
