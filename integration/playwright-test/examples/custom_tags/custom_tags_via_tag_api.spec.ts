af39c32b66251e419a0c9f9d1db856ee95753cc2
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2024-03-03 00:20:13 +0000

    fix(playwright-test): support for reporting tags registered using the new Playwright 1.42 tags API

---
+++ integration/playwright-test/examples/custom_tags/custom_tags_via_tag_api.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it } from '@serenity-js/playwright-test';
+
+describe('My feature', { tag: '@feature' }, () => {
+
+    describe('A scenario', { tag: [ '@scenario', '@issues:JIRA-1' ]}, () => {
+
+        it('passes', { tag: [ '@positive', '@issues:JIRA-2,JIRA-3' ] }, async ({ actorCalled }) => {
+            // no-op, passing
+        });
+
+        // eslint-disable-next-line mocha/no-skipped-tests
+        it.skip('manual test', { tag: '@manual' }, async ({ actorCalled }) => {});
+    });
+});
