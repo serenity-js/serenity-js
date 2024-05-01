69044c4d205bf2a526b4517d6e5e0089d592387c
jan-molak <1089173+jan-molak@users.noreply.github.com>
2024-05-01 18:22:41 +0000

    Deploying to gh-pages from @ serenity-js/serenity-js@387ba48b08bfc721d2b425250b80c7d059e2d2ed 🚀

---
+++ assets/js/18333e75.db8539b1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_documentation_serenity_js_org=self.webpackChunk_documentation_serenity_js_org||[]).push([[818],{42387:e=>{e.exports=JSON.parse('{"label":"webdriverio","permalink":"/changelog/tags/webdriverio","allTagsPath":"/changelog/tags","count":22}')}}]);
