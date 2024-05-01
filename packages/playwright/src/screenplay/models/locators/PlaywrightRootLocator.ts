7304e793db8ac06526f785827140b942be627ddb
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2024-05-01 18:22:15 +0100

    fix(playwright): ensure ExecuteScript runs in the context of the currently active iframe
    Previously, Serenity/JS Playwright integration would execute the script provided via the interaction
    to ExecuteScript in the context of the top-level frame, instead of the frame activated by the
    interaction to Switch.to(frame). With this change, interaction to Switch.to(frame) temporarily
    changes the injected JavaScript execution context to the active frame.

--- packages/playwright/src/screenplay/models/locators/PlaywrightRootLocator.ts
+++ packages/playwright/src/screenplay/models/locators/PlaywrightRootLocator.ts
@@ -1,5 +1,6 @@
 import { RootLocator } from '@serenity-js/web';
 import type * as playwright from 'playwright-core';
+import { type PageFunction } from 'playwright-core/types/structs';
 import { ensure, isDefined } from 'tiny-types';
 
 import { promised } from '../../promised';
@@ -26,15 +27,38 @@ export class PlaywrightRootLocator extends RootLocator<playwright.Locator> {
         return promised(this.currentFrame);
     }
 
+    /**
+     * Evaluates the given `pageFunction` in the context of the current frame.
+     * See [`playwright.Frame.evaluate`](https://playwright.dev/docs/api/class-frame#frame-evaluate).
+     *
+     * @param pageFunction
+     * @param arg
+     */
+    evaluate<R, Arguments>(pageFunction: PageFunction<Arguments, R>, arg: Arguments): Promise<R>;
+    evaluate<R>(pageFunction: PageFunction<void, R>, arg?: any): Promise<R> {   // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
+        return this.currentFrame.evaluate(pageFunction, arg);
+    }
+
+    /**
+     * Switches the current context to the frame identified by the given locator.
+     *
+     * @param frame
+     */
     async switchToFrame(frame: playwright.Locator): Promise<void> {
         const element = await frame.elementHandle();
         this.currentFrame = ensure('frame', await element.contentFrame(), isDefined());
     }
 
+    /**
+     * Switches the current context to the parent frame of the current frame.
+     */
     async switchToParentFrame(): Promise<void> {
         this.currentFrame = ensure('parent frame', this.currentFrame.parentFrame(), isDefined());
     }
 
+    /**
+     * Switches the context to the top-level frame.
+     */
     async switchToMainFrame(): Promise<void> {
         this.currentFrame = this.page.mainFrame();
     }
