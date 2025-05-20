f0e52dad1ca4f755360b50c41ef2c8dcbcb28929
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2025-05-20 10:36:33 +0100

    style(codefactor): use default imports for native node modules

--- packages/playwright-test/src/api/WorkerEventStreamWriter.ts
+++ packages/playwright-test/src/api/WorkerEventStreamWriter.ts
@@ -1,5 +1,5 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
+import fs from 'node:fs';
+import path from 'node:path';
 
 import type { TestCase } from '@playwright/test/reporter';
 import type { Stage, StageCrewMember } from '@serenity-js/core';
