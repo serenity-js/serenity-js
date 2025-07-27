b75dfb873b52860a4f6103daec153ca5515bbc0c
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2025-07-27 23:03:17 +0100

    ci(qlty): migrated from CodeClimate to Qlty

--- .cz-allowed-scopes.js
+++ .cz-allowed-scopes.js
@@ -30,7 +30,7 @@ module.exports.scopes = {
             'gitpod',
             'lerna',
             'renovate',
-            'codeclimate',
+            'qlty',
             'codefactor',
             'eslint',
         ];
