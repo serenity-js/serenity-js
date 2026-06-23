124f8c287e37776f4d6cd3da74bb8a65d7adcabc
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2026-06-23 01:00:42 +0100

    style(jasmine): corrected test name

--- integration/jasmine/spec/screenplay.spec.ts
+++ integration/jasmine/spec/screenplay.spec.ts
@@ -15,7 +15,7 @@ import { describe, it } from 'mocha';
 
 import { jasmine } from '../src/jasmine';
 
-describe('@serenity-js/Jasmine', function () {
+describe('Serenity/JS', function () {
 
     this.timeout(60 * 1000);
 
