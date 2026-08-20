fc3f04635ba4edabfa0120f7e9ad7428dbfbb5d9
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2026-08-20 21:30:46 +0100

    docs(html-reporter): demo error clustering

--- examples/playwright-test-todomvc/spec/failing.spec.ts
+++ examples/playwright-test-todomvc/spec/failing.spec.ts
@@ -1,22 +1,12 @@
-import { Ensure, equals } from '@serenity-js/assertions';
+import { TestCompromisedError } from '@serenity-js/core';
 import { describe, it } from '@serenity-js/playwright-test';
-
-import { startWithAnEmptyList } from './todo-list-app/TodoApp';
-import { recordItem } from './todo-list-app/TodoItem';
 
 describe('Flaky', () => {
 
     describe('Todo List App', () => {
 
-        it('should sometimes work, and sometimes not', async ({ actor }, info) => {
-            const shouldPass = Math.random() < 0.9;
-
-            await actor.attemptsTo(
-                startWithAnEmptyList(),
-
-                recordItem(`Should ${ shouldPass ? 'pass' : 'fail' } on attempt #${ info.retry + 1 }`),
-                Ensure.that(shouldPass, equals(true)),
-            );
+        it('should demonstrate a failing test', async ({ actor }, info) => {
+            throw new TestCompromisedError('Example error');
         });
     });
 });
