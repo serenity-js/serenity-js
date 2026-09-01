e76a2af8b0d8b83c86de5052a78629c4b4486d17
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2026-09-01 16:37:39 +0100

    test(examples): migrate todomvc interaction objects to .element()/.elements()
    Migrate the playwright-test-todomvc example from the child()/children()
    pattern to the new fluent .element()/.elements() APIs:
    
    - InteractionObject base class: accept Answerable<PageElement>, wrap via
      PageElement.createAdapter(), remove child()/children() methods
    - TodoApp, TodoItem, TodoList, Footer: replace this.child() with
      this.rootElement.element(), this.children() with this.rootElement.elements()
    
    Also fix an unresolvable {@link InteractionObject} JSDoc reference in
    PageElement.createAdapter().

--- examples/playwright-test-todomvc/spec/todo-list-app/InteractionObject.ts
+++ examples/playwright-test-todomvc/spec/todo-list-app/InteractionObject.ts
@@ -1,26 +1,15 @@
-import type { Answerable, Optional, QuestionAdapter } from '@serenity-js/core';
-import { PageElement, PageElements, type Selector } from '@serenity-js/web';
+import type { Answerable, Optional } from '@serenity-js/core';
+import { PageElement, type PageElementAdapter } from '@serenity-js/web';
 
 export class InteractionObject implements Optional {
 
-    constructor(
-        protected readonly rootElement: Answerable<PageElement>,
-    ) {
+    protected readonly rootElement: PageElementAdapter<unknown>;
+
+    constructor(rootElement: Answerable<PageElement>) {
+        this.rootElement = PageElement.createAdapter(rootElement);
     }
 
     isPresent(): Answerable<boolean> {
-        return (this.rootElement as QuestionAdapter<PageElement>).isPresent();
-    }
-
-    protected child(selector: Answerable<Selector>): QuestionAdapter<PageElement> {
-        return PageElement
-            .located(selector)
-            .of(this.rootElement);
-    }
-
-    protected children(selector: Answerable<Selector>): ReturnType<typeof PageElements.located> {
-        return PageElements
-            .located(selector)
-            .of(this.rootElement);
+        return this.rootElement.isPresent();
     }
 }
