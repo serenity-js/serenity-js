dd40e506fb59d59bc3814b0f7f9334b78bf39568
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2026-09-15 18:48:36 +0100

    test(playwright-test): code clean-up

--- integration/playwright-test-ct/src/UppercaseInput.serenity.ts
+++ integration/playwright-test-ct/src/UppercaseInput.serenity.ts
@@ -5,23 +5,24 @@ import { By, Enter, PageElement, PageElementAdapter, Text, Value } from '@sereni
 export class UppercaseInput<NET> {
 
     private readonly rootElement: PageElementAdapter<NET>;
+    private readonly inputField: PageElementAdapter<NET>;
+    private readonly output: PageElementAdapter<NET>;
 
     constructor(rootElement: Answerable<PageElement<NET>>) {
         this.rootElement = PageElement.createAdapter(rootElement);
+        this.inputField = this.rootElement.element(By.css('input')).describedAs('text input');
+        this.output = this.rootElement.element(By.css('.output')).describedAs('uppercase output')
     }
 
     inputValue = (): QuestionAdapter<string> =>
-        Value.of(this.rootElement.element(By.css('input')).describedAs('text input'))
+        Value.of(this.inputField)
             .describedAs('input value');
 
     outputText = (): QuestionAdapter<string> =>
-        Text.of(this.rootElement.element(By.css('.output')).describedAs('uppercase output'))
-            .describedAs('uppercase output text');
+        Text.of(this.output);
 
     enterText = (text: Answerable<string>): Task =>
         Task.where(the`#actor enters ${ text } into the uppercase input`,
-            Enter.theValue(text).into(
-                this.rootElement.element(By.css('input')).describedAs('text input'),
-            ),
+            Enter.theValue(text).into(this.inputField),
         );
 }
